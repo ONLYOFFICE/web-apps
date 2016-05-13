6d433bb6ab150f953c08f6d0c79903a8b6de6fef
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2016-05-13 14:23:09 +0300

    Список файлов сдк, подключаемый в index.html, вынесен в отдельный файл sdk_dev_scripts.

--- apps/documenteditor/main/app_dev.js
+++ apps/documenteditor/main/app_dev.js
@@ -40,8 +40,6 @@
 
 'use strict';
 var reqerr;
-require.config();
-
 require.config({
     // The shim config allows us to configure dependencies for
     // scripts that do not call define() to register a module
