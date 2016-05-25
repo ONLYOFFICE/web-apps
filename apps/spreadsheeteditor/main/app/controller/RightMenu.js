5b56120188798973cd86dd52b8a28644c694df2a
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2016-05-25 17:09:55 +0300

    [SSE] дополнение к багу 32505 (не обрабатывать событие asc_onSelectionChanged, если находимся в режиме выделения диапазона ячеек)

--- apps/spreadsheeteditor/main/app/controller/RightMenu.js
+++ apps/spreadsheeteditor/main/app/controller/RightMenu.js
@@ -85,6 +85,7 @@ define([
             this.api = api;
             this.api.asc_registerCallback('asc_onCoAuthoringDisconnect',_.bind(this.onCoAuthoringDisconnect, this));
             Common.NotificationCenter.on('api:disconnect',              _.bind(this.onCoAuthoringDisconnect, this));
+            Common.NotificationCenter.on('cells:range',                 _.bind(this.onCellsRange, this));
         },
 
         setMode: function(mode) {
@@ -103,6 +104,8 @@ define([
         },
 
         onSelectionChanged: function(info) {
+            if (this.rangeSelectionMode) return;
+            
             var SelectedObjects = [],
                 selectType = info.asc_getFlags().asc_getSelectionType(),
                 formatTableInfo = info.asc_getFormatTableInfo();
@@ -318,6 +321,10 @@ define([
                     this.onSelectionChanged(this.api.asc_getCellInfo());
                 }
             }
+        },
+
+        onCellsRange: function(status) {
+            this.rangeSelectionMode = (status != Asc.c_oAscSelectionDialogType.None);
         }
     });
 });
