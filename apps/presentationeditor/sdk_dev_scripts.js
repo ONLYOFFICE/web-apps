be682b37718e1739696d477647b1782b074e0f0b
GoshaZotov <Igor.Zotov@onlyoffice.com>
2016-06-27 13:47:10 +0300

    add text_input.js script

--- apps/presentationeditor/sdk_dev_scripts.js
+++ apps/presentationeditor/sdk_dev_scripts.js
@@ -147,5 +147,6 @@ var sdk_dev_scrpipts = [
     "../../../../sdkjs/common/applyDocumentChanges.js",
     "../../../../sdkjs/common/Drawings/Format/OleObject.js",
     "../../../../sdkjs/common/clipboard_base.js",
-    "../../../../sdkjs/common/plugins.js"
+    "../../../../sdkjs/common/plugins.js",
+    "../../../../sdkjs/common/text_input.js"
 ];
