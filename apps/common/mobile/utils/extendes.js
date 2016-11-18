bfecf9d525aff0f3f4f01a07b0639cd57d4a2d7f
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2016-11-18 19:12:10 +0300

    moved module dependencies

--- apps/common/mobile/utils/extendes.js
+++ apps/common/mobile/utils/extendes.js
@@ -39,44 +39,45 @@
  *
  */
 
-(function() {
-
-    //Extend jQuery functions
-    jQuery.fn.extend( {
-        single: function(types, selector, data, fn) {
-            return this.off(types, fn).on(types, selector, data, fn);
-        }
-    });
-
-    //Extend Dom7 functions
-    var methods = ['addClass', 'toggleClass', 'removeClass'];
-
-    _.each(methods, function (method, index) {
-        var originalMethod = Dom7.fn[method];
-
-        Dom7.fn[method] = function(className) {
-            var result = originalMethod.apply(this, arguments);
-            this.trigger(method, className);
-            return result;
-        };
-    });
-
-    //Extend Underscope functions
-    _.buffered = function(func, buffer, scope, args) {
-        var timerId;
-
-        return function() {
-            var callArgs = args || Array.prototype.slice.call(arguments, 0),
-                me = scope || this;
-
-            if (timerId) {
-                clearTimeout(timerId);
+define(
+    ['jquery','underscore','framework7'],
+    function () {
+        //Extend jQuery functions
+        jQuery.fn.extend( {
+            single: function(types, selector, data, fn) {
+                return this.off(types, fn).on(types, selector, data, fn);
             }
+        });
 
-            timerId = setTimeout(function(){
-                func.apply(me, callArgs);
-            }, buffer);
+        //Extend Dom7 functions
+        var methods = ['addClass', 'toggleClass', 'removeClass'];
+
+        _.each(methods, function (method, index) {
+            var originalMethod = Dom7.fn[method];
+
+            Dom7.fn[method] = function(className) {
+                var result = originalMethod.apply(this, arguments);
+                this.trigger(method, className);
+                return result;
+            };
+        });
+
+        //Extend Underscope functions
+        _.buffered = function(func, buffer, scope, args) {
+            var timerId;
+
+            return function() {
+                var callArgs = args || Array.prototype.slice.call(arguments, 0),
+                    me = scope || this;
+
+                if (timerId) {
+                    clearTimeout(timerId);
+                }
+
+                timerId = setTimeout(function(){
+                    func.apply(me, callArgs);
+                }, buffer);
+            };
         };
-    };
-
-})();
+    }
+);
