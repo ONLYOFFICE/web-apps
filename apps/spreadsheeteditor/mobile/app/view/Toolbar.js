42e31d1811d1f2065ae9d2e55cb6a171a4d97d71
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2016-12-05 16:22:52 +0300

    [SSE mobile] added settings

--- apps/spreadsheeteditor/mobile/app/view/Toolbar.js
+++ apps/spreadsheeteditor/mobile/app/view/Toolbar.js
@@ -68,15 +68,6 @@ define([
             // Set innerHTML and get the references to the DOM elements
             initialize: function() {
                 var me = this;
-
-                Common.NotificationCenter.on('readermode:change', function (reader) {
-                    if (reader) {
-                        me.hideSearch();
-                        $('#toolbar-search').addClass('disabled');
-                    } else {
-                        $('#toolbar-search').removeClass('disabled');
-                    }
-                });
             },
 
             // Render layout
@@ -197,7 +188,7 @@ define([
 
             // Settings
             showSettings: function () {
-                // SSE.getController('Settings').showModal();
+                SSE.getController('Settings').showModal();
             }
         }
     })());
