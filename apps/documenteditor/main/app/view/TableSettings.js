7deaa2888bd5a8c33b153ad71be3475c11921161
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2017-03-28 10:47:36 +0300

    [DE] Update table settings in strict mode: change table style by one user and getting changes by another user (combodataview didn't update.).

--- apps/documenteditor/main/app/view/TableSettings.js
+++ apps/documenteditor/main/app/view/TableSettings.js
@@ -430,7 +430,7 @@ define([
             if (this._initSettings)
                 this.createDelayedElements();
 
-            this.disableControls(this._locked);
+            var updateState = this.disableControls(this._locked); // need to update combodataview after disabled state
 
             if (props )
             {
@@ -451,7 +451,7 @@ define([
 
                 //for table-template
                 value = props.get_TableStyle();
-                if (this._state.TemplateId!==value || this._isTemplatesChanged) {
+                if (this._state.TemplateId!==value || this._isTemplatesChanged || updateState) {
                     this.cmbTableTemplate.suspendEvents();
                     var rec = this.cmbTableTemplate.menuPicker.store.findWhere({
                         templateId: value
@@ -773,6 +773,7 @@ define([
                     item.setDisabled(disable);
                 });
                 this.linkAdvanced.toggleClass('disabled', disable);
+                return !disable;
             }
         },
 
