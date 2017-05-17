523bb4e82f7f2b47b86f3975ac5a2919126aa0e8
SergeyLuzyanin <Sergey.Luzyanin@onlyoffice.com>
2017-05-17 16:36:57 +0300

    ParagraphContentBase

--- apps/presentationeditor/sdk_dev_scripts.js
+++ apps/presentationeditor/sdk_dev_scripts.js
@@ -116,6 +116,7 @@ var sdk_dev_scrpipts = [
     "../../../../sdkjs/slide/Editor/Format/NotesMaster.js",
     "../../../../sdkjs/slide/Editor/Format/Notes.js",
     "../../../../sdkjs/word/Editor/ParagraphContent.js",
+    "../../../../sdkjs/word/Editor/ParagraphContentBase.js",
 	"../../../../sdkjs/word/Editor/Paragraph/ParaTextPr.js",
 	"../../../../sdkjs/word/Editor/Paragraph/ParaTextPrChanges.js",
 	"../../../../sdkjs/word/Editor/Paragraph/ParaDrawing.js",
