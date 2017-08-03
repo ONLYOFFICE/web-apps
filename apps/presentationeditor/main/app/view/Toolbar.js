1774b33b45fc27b2f620eafdaed6f5104b88a069
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2017-08-03 16:36:14 +0300

    [PE] Fix Bug 35510.

--- apps/presentationeditor/main/app/view/Toolbar.js
+++ apps/presentationeditor/main/app/view/Toolbar.js
@@ -794,7 +794,7 @@ define([
                     enableKeyEvents: true,
                     itemHeight  : 38,
                     hint: this.tipSlideTheme,
-                    lock: [_set.lostConnect, _set.noSlides],
+                    lock: [_set.themeLock, _set.lostConnect, _set.noSlides],
                     beforeOpenHandler: function(e) {
                         var cmp = this,
                             menu = cmp.openButton.menu,
