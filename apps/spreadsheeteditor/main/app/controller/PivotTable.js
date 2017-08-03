193b95b7dcbe81a76d03d7d4cf02aaee79a2d1ed
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2017-08-03 15:28:49 +0300

    [SSE] Update grand totals.

--- apps/spreadsheeteditor/main/app/controller/PivotTable.js
+++ apps/spreadsheeteditor/main/app/controller/PivotTable.js
@@ -182,8 +182,8 @@ define([
         onPivotGrandTotals: function(type){
             if (this.api) {
                 var props = this._originalProps.asc_clone();
-                props.asc_setColGrandTotals(type == 1 || type == 3);
-                props.asc_setRowGrandTotals(type == 1 || type == 2);
+                props.asc_setColGrandTotals(type == 1 || type == 2);
+                props.asc_setRowGrandTotals(type == 1 || type == 3);
                 this._originalProps.asc_set(this.api, props);
             }
             Common.NotificationCenter.trigger('edit:complete', this);
