8900b912d755e708b9d3587525dc984fc67705c6
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2017-08-17 17:32:37 +0300

    User icon isn't displayed in FF, Safari on retina.

--- apps/common/main/lib/view/Header.js
+++ apps/common/main/lib/view/Header.js
@@ -81,7 +81,7 @@ define([
                                 // '<span class="btn-slot text" id="slot-btn-users"></span>' +
                                 '<section id="tlb-box-users" class="box-cousers dropdown"">' +
                                     '<div class="btn-users">' +
-                                        '<svg class="icon"><use href="#svg-btn-users"></use></svg>' +
+                                        '<svg class="icon"><use xlink:href="#svg-btn-users"></use></svg>' +
                                         '<label class="caption">&plus;</label>' +
                                     '</div>' +
                                     '<div class="cousers-menu dropdown-menu">' +
