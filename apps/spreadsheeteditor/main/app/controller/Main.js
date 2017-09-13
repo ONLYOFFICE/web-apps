a147880f22d5921f84025a3ec2003c94eb62a4d4
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2017-09-13 14:35:20 +0300

    Debug autostart plugins.

--- apps/spreadsheeteditor/main/app/controller/Main.js
+++ apps/spreadsheeteditor/main/app/controller/Main.js
@@ -1850,12 +1850,17 @@ define([
                     }
                 });
 
-                if (arr.length>0)
+                if (arr.length>0) {
+                    var autostart = plugins.autostart || plugins.autoStartGuid;
+                    if (typeof (autostart) == 'string')
+                        autostart = [autostart];
+                    plugins.autoStartGuid && console.warn("Obsolete: The autoStartGuid parameter is deprecated. Please check the documentation for new plugin connection configuration.");
+
                     this.updatePluginsList({
-                        autoStartGuid: plugins.autoStartGuid,
-                        autostart: plugins.autostart,
+                        autostart: autostart,
                         pluginsData: arr
                     }, !!uiCustomize);
+                }
             },
 
             updatePluginsList: function(plugins, uiCustomize) {
@@ -1919,10 +1924,7 @@ define([
                 }
                 if (this.appOptions.canPlugins) {
                     this.getApplication().getController('Common.Controllers.Plugins').setMode(this.appOptions);
-                    if (plugins.autoStartGuid) {
-                        console.warn("Obsolete: The autoStartGuid parameter is deprecated. Please check the documentation for new plugin connection configuration.");
-                        this.api.asc_pluginRun(plugins.autoStartGuid, 0, '');
-                    } else if (plugins.autostart && plugins.autostart.length>0) {
+                    if (plugins.autostart && plugins.autostart.length>0) {
                         // run array of plugins
                         this.api.asc_pluginRun(plugins.autostart[0], 0, '');
                     }
