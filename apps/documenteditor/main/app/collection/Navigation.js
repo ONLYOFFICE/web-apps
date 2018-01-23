a9cc7112610b9dc0b9b290ae0ad4836f0b1694f5
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2018-01-23 17:25:05 +0300

    [DE] Fix bug on loading app.

--- apps/documenteditor/main/app/collection/Navigation.js
+++ apps/documenteditor/main/app/collection/Navigation.js
@@ -35,14 +35,14 @@
  * Date: 14.12.17
  */
 
-DE.Collections = DE.Collections || {};
-
 define([
     'underscore',
     'backbone',
     'common/main/lib/component/TreeView'
 ], function(_, Backbone){
     'use strict';
+
+    DE.Collections = DE.Collections || {};
 
     DE.Collections.Navigation = Common.UI.TreeViewStore.extend({
         model: Common.UI.TreeViewModel
