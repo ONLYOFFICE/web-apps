680f5498468ad2dc79528279cf4625e1c8589099
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2018-05-07 15:47:44 +0300

    [DE] Fix setting option Realtime collaboration changes to "View All"

--- apps/documenteditor/main/app/controller/LeftMenu.js
+++ apps/documenteditor/main/app/controller/LeftMenu.js
@@ -321,13 +321,13 @@ define([
                 this.api.asc_SetFastCollaborative(fast_coauth);
 
                 value = Common.localStorage.getItem((fast_coauth) ? "de-settings-showchanges-fast" : "de-settings-showchanges-strict");
+                Common.Utils.InternalSettings.set((fast_coauth) ? "de-settings-showchanges-fast" : "de-settings-showchanges-strict", value);
                 switch(value) {
                 case 'all': value = Asc.c_oAscCollaborativeMarksShowType.All; break;
                 case 'none': value = Asc.c_oAscCollaborativeMarksShowType.None; break;
                 case 'last': value = Asc.c_oAscCollaborativeMarksShowType.LastChanges; break;
                 default: value = (fast_coauth) ? Asc.c_oAscCollaborativeMarksShowType.None : Asc.c_oAscCollaborativeMarksShowType.LastChanges;
                 }
-                Common.Utils.InternalSettings.set((fast_coauth) ? "de-settings-showchanges-fast" : "de-settings-showchanges-strict", value);
                 this.api.SetCollaborativeMarksShowType(value);
             }
 
