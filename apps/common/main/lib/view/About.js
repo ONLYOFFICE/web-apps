7e2c0fac5152847f85f22fad9425efac2a0b651b
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2018-11-09 12:48:48 +0300

    Bug 39644

--- apps/common/main/lib/view/About.js
+++ apps/common/main/lib/view/About.js
@@ -77,7 +77,7 @@ define([
                     '<tr>',
                         '<td colspan="3" align="center" class="padding-small">',
                         '<label class="asc-about-desc-name">' + this.txtAddress + '</label>',
-                        '<label class="asc-about-desc">' + this.txtAscAddress + '</label>',
+                        '<label class="asc-about-desc">' + "20A-12 Ernesta Birznieka-Upisha street, Riga, Latvia, EU, LV-1050" + '</label>',
                         '</td>',
                     '</tr>',
                     '<tr>',
@@ -247,7 +247,6 @@ define([
         txtLicensor: 'LICENSOR',
         txtLicensee: 'LICENSEE',
         txtAddress: 'address: ',
-        txtAscAddress: 'Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021',
         txtMail: 'email: ',
         txtTel: 'tel.: ',
         txtEdition: 'Integration Edition '
