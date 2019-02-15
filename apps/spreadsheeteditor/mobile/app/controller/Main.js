07ad684ba18a8216296894f4e2e0b190c25491b8
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2019-02-15 16:43:42 +0300

    [Mobile] Add c_oAscError.ID.EditingError error

--- apps/spreadsheeteditor/mobile/app/controller/Main.js
+++ apps/spreadsheeteditor/mobile/app/controller/Main.js
@@ -981,6 +981,10 @@ define([
                         config.msg = this.errorChangeArray;
                         break;
 
+                    case Asc.c_oAscError.ID.EditingError:
+                        config.msg = this.errorEditingDownloadas;
+                        break;
+
                     default:
                         config.msg = this.errorDefaultMessage.replace('%1', id);
                         break;
@@ -1540,7 +1544,8 @@ define([
             errorPrintMaxPagesCount: 'Unfortunately, it’s not possible to print more than 1500 pages at once in the current version of the program.<br>This restriction will be eliminated in upcoming releases.',
             closeButtonText: 'Close File',
             scriptLoadError: 'The connection is too slow, some of the components could not be loaded. Please reload the page.',
-            errorChangeArray: 'You cannot change part of an array.'
+            errorChangeArray: 'You cannot change part of an array.',
+            errorEditingDownloadas: 'An error occurred during the work with the document.<br>Use the \'Download\' option to save the file backup copy to your computer hard drive.'
         }
     })(), SSE.Controllers.Main || {}))
 });
