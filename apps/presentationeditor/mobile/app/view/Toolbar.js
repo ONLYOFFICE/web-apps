d56b256cb3b6ae612677f340a120d766667ba89d
Julia Svinareva <julia.svinaryova@onlyoffice.com>
2019-06-04 09:46:48 +0300

    [PE mobile] Add Collaboration into Toolbar

--- apps/presentationeditor/mobile/app/view/Toolbar.js
+++ apps/presentationeditor/mobile/app/view/Toolbar.js
@@ -64,7 +64,8 @@ define([
                 "click #toolbar-add"        : "showInserts",
                 "click #toolbar-settings"   : "showSettings",
                 "click #toolbar-preview"    : "showPreview",
-                "click #toolbar-edit-document": "editDocument"
+                "click #toolbar-edit-document": "editDocument",
+                "click #toolbar-collaboration" : "showCollaboration"
             },
 
             // Set innerHTML and get the references to the DOM elements
@@ -101,7 +102,7 @@ define([
 
             setMode: function (mode) {
                 if (mode.isEdit) {
-                    $('#toolbar-edit, #toolbar-add, #toolbar-undo, #toolbar-redo').show();
+                    $('#toolbar-edit, #toolbar-add, #toolbar-undo, #toolbar-redo, #toolbar-collaboration').show();
                 } else if (mode.canEdit && mode.canRequestEditRights){
                     $('#toolbar-edit-document').show();
                 }
@@ -157,6 +158,11 @@ define([
 
             editDocument: function () {
                 Common.Gateway.requestEditRights();
+            },
+
+            //Collaboration
+            showCollaboration: function () {
+                PE.getController('Collaboration').showModal();
             },
 
             textBack: 'Back'
