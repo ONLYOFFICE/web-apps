fca164082065e146e76bb7f4fce6755e670628b7
Julia Svinareva <julia.svinaryova@onlyoffice.com>
2019-07-18 13:35:35 +0300

    [Common mobile] Fix bug with hide overlay

--- apps/common/mobile/lib/controller/Collaboration.js
+++ apps/common/mobile/lib/controller/Collaboration.js
@@ -163,6 +163,22 @@ define([
                     domCache: true
                 });
 
+                if (!Common.SharedSettings.get('phone')) {
+                    this.picker = $$(modalView);
+                    var $overlay = $('.modal-overlay');
+
+                    $$(this.picker).on('opened', function () {
+                        $overlay.on('removeClass', function () {
+                            if (!$overlay.hasClass('modal-overlay-visible')) {
+                                $overlay.addClass('modal-overlay-visible')
+                            }
+                        });
+                    }).on('close', function () {
+                        $overlay.off('removeClass');
+                        $overlay.removeClass('modal-overlay-visible')
+                    });
+                }
+
                 Common.NotificationCenter.trigger('collaborationcontainer:show');
                 this.onPageShow(this.getView('Common.Views.Collaboration'));
 
