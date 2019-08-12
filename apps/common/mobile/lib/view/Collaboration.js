03c94b12ebebf588c9d730cb0d03d64c05e90458
Julia Svinareva <julia.svinaryova@onlyoffice.com>
2019-08-12 10:50:44 +0300

    [Common mobile] Add no comment text

--- apps/common/mobile/lib/view/Collaboration.js
+++ apps/common/mobile/lib/view/Collaboration.js
@@ -147,41 +147,52 @@ define([
             },
 
             renderComments: function (comments) {
-                var $listComments = $('#comments-list'),
-                    items = [];
-
-                _.each(comments, function (comment) {
-                    var itemTemplate = [
-                        '<li class="comment item-content">',
-                        '<div class="item-inner">',
-                        '<p class="user-name"><%= item.username %></p>',
-                        '<p class="comment-date"><%= item.date %></p>',
-                        '<% if(item.quote) {%>',
-                        '<p class="comment-quote" data-id="<%= item.uid %>"><%= item.quote %></p>',
-                        '<% } %>',
-                        '<p class="comment-text"><%= item.comment %></p>',
-                        '<% if(replys > 0) {%>',
-                        '<ul class="list-reply">',
-                        '<% _.each(item.replys, function (reply) { %>',
-                        '<li class="reply-item">',
-                        '<p class="user-name"><%= reply.username %></p>',
-                        '<p class="reply-date"><%= reply.date %></p>',
-                        '<p class="reply-text"><%= reply.reply %></p>',
-                        '</li>',
-                        '<% }); %>',
-                        '</ul>',
-                        '<% } %>',
-                        '</div>',
-                        '</li>'
-                    ].join('');
-                    items.push(_.template(itemTemplate)({
-                        android: Framework7.prototype.device.android,
-                        item: comment,
-                        replys: comment.replys.length
-                    }));
-                });
-
-                $listComments.html(items);
+                var $pageComments = $('.page-comments .page-content');
+                if (!comments) {
+                    if ($('.comment').length > 0) {
+                        $('.comment').remove();
+                    }
+                    var textNoComments = !!window.DE ? this.textNoDocumentComments : !!window.PE ? this.textNoPresentationComments : this.textNoSpreadsheetComments,
+                        template = '<div id="no-comments" style="text-align: center; margin-top: 35px;">' + textNoComments + '</div>';
+                    $pageComments.append(_.template(template));
+                } else {
+                    if ($('#no-comments').length > 0) {
+                        $('#no-comments').remove();
+                    }
+                    var $listComments = $('#comments-list'),
+                        items = [];
+                    _.each(comments, function (comment) {
+                        var itemTemplate = [
+                            '<li class="comment item-content">',
+                            '<div class="item-inner">',
+                            '<p class="user-name"><%= item.username %></p>',
+                            '<p class="comment-date"><%= item.date %></p>',
+                            '<% if(item.quote) {%>',
+                            '<p class="comment-quote" data-id="<%= item.uid %>"><%= item.quote %></p>',
+                            '<% } %>',
+                            '<p class="comment-text"><%= item.comment %></p>',
+                            '<% if(replys > 0) {%>',
+                            '<ul class="list-reply">',
+                            '<% _.each(item.replys, function (reply) { %>',
+                            '<li class="reply-item">',
+                            '<p class="user-name"><%= reply.username %></p>',
+                            '<p class="reply-date"><%= reply.date %></p>',
+                            '<p class="reply-text"><%= reply.reply %></p>',
+                            '</li>',
+                            '<% }); %>',
+                            '</ul>',
+                            '<% } %>',
+                            '</div>',
+                            '</li>'
+                        ].join('');
+                        items.push(_.template(itemTemplate)({
+                            android: Framework7.prototype.device.android,
+                            item: comment,
+                            replys: comment.replys.length,
+                        }));
+                    });
+                    $listComments.html(items);
+                }
             },
 
 
@@ -198,7 +209,11 @@ define([
             textFinal: 'Final',
             textOriginal: 'Original',
             textChange: 'Review Change',
-            textEditUsers: 'Users'
+            textEditUsers: 'Users',
+            textNoDocumentComments: 'This document doesn\'t contain comments',
+            textNoSpreadsheetComments: 'This spreadsheet doesn\'t contain comments',
+            textNoPresentationComments: 'This presentation doesn\'t contain comments'
+
 
         }
     })(), Common.Views.Collaboration || {}))
