6a738e70f20727ba6a7bf28556a0b5174d4cca6e
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2019-10-30 14:04:00 +0300

    [DE][SSE] Send "embedded" parameter to sdk

--- apps/documenteditor/embed/js/ApplicationController.js
+++ apps/documenteditor/embed/js/ApplicationController.js
@@ -513,7 +513,8 @@ DE.ApplicationController = new(function(){
 
         window["flat_desine"] = true;
         api = new Asc.asc_docs_api({
-            'id-view'  : 'editor_sdk'
+            'id-view'  : 'editor_sdk',
+            'embedded' : true
         });
 
         if (api){
