3b43b1d5430338d1a66cf1895d503398a30a025c
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2020-01-13 11:07:23 +0300

    [DE] Fix paragraph indents (#293)

--- apps/documenteditor/main/app/view/ParagraphSettingsAdvanced.js
+++ apps/documenteditor/main/app/view/ParagraphSettingsAdvanced.js
@@ -1414,14 +1414,14 @@ define([    'text!documenteditor/main/app/template/ParagraphSettingsAdvanced.tem
                 if (this._changedProps.get_Ind()===null || this._changedProps.get_Ind()===undefined)
                     this._changedProps.put_Ind(new Asc.asc_CParagraphInd());
                 var value = Common.Utils.Metric.fnRecalcToMM(field.getNumberValue());
-                if (this.CurSpecial === c_paragraphSpecial.HANGING) {
-                    value = -value;
-                } else if (this.CurSpecial === c_paragraphSpecial.NONE_SPECIAL && value > 0 )  {
+                if (this.CurSpecial === c_paragraphSpecial.NONE_SPECIAL && value > 0 )  {
                     this.CurSpecial = c_paragraphSpecial.FIRST_LINE;
                     this.cmbSpecial.setValue(c_paragraphSpecial.FIRST_LINE);
                 } else if (value === 0) {
                     this.CurSpecial = c_paragraphSpecial.NONE_SPECIAL;
                     this.cmbSpecial.setValue(c_paragraphSpecial.NONE_SPECIAL);
+                } else if (this.CurSpecial === c_paragraphSpecial.HANGING) {
+                    value = -value;
                 }
                 this._changedProps.get_Ind().put_FirstLine(value);
             }
