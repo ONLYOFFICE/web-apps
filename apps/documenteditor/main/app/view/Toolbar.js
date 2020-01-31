f4aee180e97882d477c2ae28d904ddbaec56cc2c
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2020-01-31 13:53:58 +0300

    [DE] Use svg icons for chart type

--- apps/documenteditor/main/app/view/Toolbar.js
+++ apps/documenteditor/main/app/view/Toolbar.js
@@ -1676,7 +1676,7 @@ define([
                         restoreHeight: 421,
                         groups: new Common.UI.DataViewGroupStore(Common.define.chartData.getChartGroupData(true)),
                         store: new Common.UI.DataViewStore(Common.define.chartData.getChartData()),
-                        itemTemplate: _.template('<div id="<%= id %>" class="item-chartlist <%= iconCls %>"></div>')
+                        itemTemplate: _.template('<div id="<%= id %>" class="item-chartlist"><svg width="40" height="40" class=\"icon\"><use xlink:href=\"#chart-<%= iconCls %>\"></use></svg></div>')
                     });
                     picker.on('item:click', function (picker, item, record, e) {
                         if (record)
