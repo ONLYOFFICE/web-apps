1a88f7698b71ab393f78a0d02566ea614974da88
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2020-03-25 13:00:31 +0300

    [SSE] Check support for pivot tables

--- apps/spreadsheeteditor/main/app/controller/Main.js
+++ apps/spreadsheeteditor/main/app/controller/Main.js
@@ -352,7 +352,7 @@ define([
                 this.appOptions.canRequestSharingSettings = this.editorConfig.canRequestSharingSettings;
                 this.appOptions.mentionShare = !((typeof (this.appOptions.customization) == 'object') && (this.appOptions.customization.mentionShare==false));
                 this.appOptions.canMakeActionLink = this.editorConfig.canMakeActionLink;
-                this.appOptions.canFeaturePivot = !!this.api.asc_isSupportFeature("pivot");
+                this.appOptions.canFeaturePivot = !!this.api.asc_isSupportFeature("pivot-tables");
 
                 this.headerView = this.getApplication().getController('Viewport').getView('Common.Views.Header');
                 this.headerView.setCanBack(this.appOptions.canBackToFolder === true, (this.appOptions.canBackToFolder) ? this.editorConfig.customization.goback.text : '')
