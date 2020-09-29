ce969e9ca63463edca76ad792f63eb445c608897
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2020-09-29 21:57:01 +0300

    tune packages to use mobx

--- vendor/framework7-react/babel.config.js
+++ vendor/framework7-react/babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-decorators', {'legacy': true }],
+    ['@babel/plugin-proposal-class-properties',{'loose':true}],
   ],
 };
