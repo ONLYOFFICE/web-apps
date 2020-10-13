2b770c5547420d042afd0fc62043034378e65ba8
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2020-10-13 16:58:36 +0300

    [DE] cleaned up code

--- apps/documenteditor/mobile/src/components/app.jsx
+++ apps/documenteditor/mobile/src/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {App,Panel,Views,View,Popup,Page,Navbar,Toolbar,NavRight,Link,Block,BlockTitle,LoginScreen,LoginScreenTitle,List,ListItem,ListInput,ListButton,BlockFooter} from 'framework7-react';
+import {App,Panel,Views,View,Popup,Page,Navbar,Toolbar,NavRight,Link,Block,BlockTitle,List,ListItem,ListInput,ListButton,BlockFooter} from 'framework7-react';
 
 import routes from '../js/routes';
 
@@ -19,14 +19,9 @@ export default class extends React.Component {
         name: 'Desktop Editor', // App name
         theme: 'auto', // Automatic theme detection
 
-
-
         // App routes
         routes: routes,
       },
-      // Login screen demo data
-      username: '',
-      password: '',
     }
 
       Common.Notifications = new Notifications();
@@ -83,11 +78,7 @@ export default class extends React.Component {
       </App>
     )
   }
-  alertLoginData() {
-    this.$f7.dialog.alert('Username: ' + this.state.username + '<br>Password: ' + this.state.password, () => {
-      this.$f7.loginScreen.close();
-    });
-  }
+
     componentDidMount() {
         this.$f7ready((f7) => {
         // Call F7 APIs here
