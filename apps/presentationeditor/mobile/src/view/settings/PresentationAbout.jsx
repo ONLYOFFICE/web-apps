2f294ea4cd7fe5cc19f009f9728635332a26ce94
SergeyEzhin <ezhin@mail.ru>
2020-12-21 14:44:25 +0300

    [PE mobile] Edited Presentation About

--- apps/presentationeditor/mobile/src/view/settings/PresentationAbout.jsx
+++ apps/presentationeditor/mobile/src/view/settings/PresentationAbout.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { observer, inject } from "mobx-react";
-import { Page, Navbar } from "framework7-react";
+import { Page, Navbar, Link } from "framework7-react";
 import { useTranslation } from "react-i18next";
 
 const PagePresentationAbout = props => {
@@ -9,23 +9,13 @@ const PagePresentationAbout = props => {
     const store = props.storeAppOptions;
     const isCanBranding = store.canBranding;
     const licInfo = isCanBranding ? store.customization : null;
-    let customer;
-    let nameCustomer;
-    let addressCustomer;
-    let mailCustomer;
-    let urlCustomer;
-    let infoCustomer;
-    let logoCustomer;
-
-    if(licInfo) {
-        customer = licInfo.customer;
-        nameCustomer = customer.name;
-        addressCustomer = customer.address;
-        mailCustomer = customer.mail;
-        urlCustomer = customer.www;
-        infoCustomer = customer.info;
-        logoCustomer = customer.logo;
-    }
+    const customer = licInfo ? licInfo.customer : null;
+    const nameCustomer = customer ? customer.name : null;
+    const mailCustomer = customer ? customer.mail : null;
+    const addressCustomer = customer ? customer.address : null;
+    const urlCustomer = customer ? customer.www : null;
+    const infoCustomer = customer ? customer.info : null;
+    const logoCustomer = customer ? customer.logo : null;
 
     console.log(store);
     console.log(isCanBranding);
@@ -34,7 +24,7 @@ const PagePresentationAbout = props => {
         <Page className="about">
             <Navbar title={_t.textAbout} backLink={_t.textBack} />
             <div className="content-block">
-                {licInfo && typeof licInfo == 'object' && typeof(licInfo.customer)=='object' ? null : (
+                {licInfo && typeof licInfo == 'object' && typeof(customer)=='object' ? null : (
                     <div className="logo"></div>
                 )}
                 {logoCustomer && logoCustomer.length ? (
@@ -54,27 +44,27 @@ const PagePresentationAbout = props => {
                 {addressCustomer && addressCustomer.length ? (
                     <p>
                         <label>{_t.textAddress}</label>
-                        <a id="settings-about-address" className="external" href="#">{addressCustomer}</a>
+                        <Link id="settings-about-address" className="external">{addressCustomer}</Link>
                     </p>
                 ) : null}
                 {mailCustomer && mailCustomer.length ? (
                     <p>
                         <label>{_t.textEmail}</label>
-                        <a id="settings-about-email" className="external" target="_blank" href={"mailto:"+mailCustomer}>{mailCustomer}</a>
+                        <Link id="settings-about-email" className="external" target="_blank" href={"mailto:"+mailCustomer}>{mailCustomer}</Link>
                     </p>
                 ) : null}
-                {licInfo && typeof licInfo == 'object' && typeof(licInfo.customer)=='object' ? null : (
+                {licInfo && typeof licInfo == 'object' && typeof(customer)=='object' ? null : (
                     <p>
                         <label>{_t.textTel}</label>
-                        <a id="settings-about-tel" className="external" target="_blank" href="tel:+37163399867">+371 633-99867</a>
+                        <Link id="settings-about-tel" className="external" target="_blank" href="tel:+37163399867">+371 633-99867</Link>
                     </p>
                 )}
                 {urlCustomer && urlCustomer.length ? (
                     <p>
-                        <a id="settings-about-url" className="external" target="_blank" 
+                        <Link id="settings-about-url" className="external" target="_blank" 
                             href={!/^https?:\/{2}/i.test(urlCustomer) ? "http:\/\/" : '' + urlCustomer}>
                             {urlCustomer}
-                        </a>
+                        </Link>
                     </p>
                 ) : null} 
                 {infoCustomer && infoCustomer.length ? (
@@ -83,7 +73,7 @@ const PagePresentationAbout = props => {
                     </p>
                 ) : null}
             </div>
-            {licInfo && typeof licInfo == 'object' && typeof(licInfo.customer)=='object' ? (
+            {licInfo && typeof licInfo == 'object' && typeof(customer)=='object' ? (
                 <div className="content-block" id="settings-about-licensor">
                     <div className="content-block-inner"></div>
                     <p>
@@ -91,7 +81,7 @@ const PagePresentationAbout = props => {
                     </p>
                     <h3 className="vendor">Ascensio System SIA</h3>
                     <p>
-                        <a className="external" target="_blank" href="www.onlyoffice.com">www.onlyoffice.com</a>
+                        <Link className="external" target="_blank" href="www.onlyoffice.com">www.onlyoffice.com</Link>
                     </p>
                 </div>
             ) : null}
