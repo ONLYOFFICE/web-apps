e9abada366bafebb1a6faed14feff2f024d2caa5
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2020-12-27 16:28:16 +0300

    [DE] remove unused elements

--- apps/documenteditor/mobile/src/view/app.jsx
+++ apps/documenteditor/mobile/src/view/app.jsx
@@ -31,51 +31,9 @@ export default class extends React.Component {
     return (
       <App params={ this.state.f7params } >
 
-        {/* Left panel with cover effect when hidden */}
-        <Panel left cover themeDark visibleBreakpoint={960}>
-          <View>
-            <Page>
-              <Navbar title="Left Panel"/>
-              <BlockTitle>Left View Navigation</BlockTitle>
-              <List>
-                <ListItem link="/left-page-1/" title="Left Page 1"/>
-                <ListItem link="/left-page-2/" title="Left Page 2"/>
-              </List>
-            </Page>
-          </View>
-        </Panel>
-
-
-        {/* Right panel with reveal effect*/}
-        <Panel right reveal themeDark>
-          <View>
-            <Page>
-              <Navbar title="Right Panel"/>
-              <Block>Right panel content goes here</Block>
-            </Page>
-          </View>
-        </Panel>
-
-
         {/* Your main view, should have "view-main" class */}
         <View main className="safe-areas" url="/" />
           <MainController />
-
-        {/* Popup */}
-        <Popup id="my-popup">
-          <View>
-            <Page>
-              <Navbar title="Popup">
-                <NavRight>
-                  <Link popupClose>Close</Link>
-                </NavRight>
-              </Navbar>
-              <Block>
-                <p>Popup content goes here.</p>
-              </Block>
-            </Page>
-          </View>
-        </Popup>
       </App>
     )
   }
