e8f2c61f8368043da567d74e9afcaab1a9e7a629
JuliaSvinareva <Julia.Svinaryova@onlyoffice.com>
2021-01-20 20:59:19 +0300

    [DE mobile] Added the ability to add shapes

--- apps/documenteditor/mobile/src/view/add/AddShape.jsx
+++ apps/documenteditor/mobile/src/view/add/AddShape.jsx
@@ -1,15 +1,28 @@
-import React, {Fragment, useState} from 'react';
+import React from 'react';
 import {observer, inject} from "mobx-react";
-import {Page, Navbar, List, ListItem, ListButton, Row, BlockTitle, Range, Toggle, Icon} from 'framework7-react';
-import { useTranslation } from 'react-i18next';
-import {Device} from '../../../../../common/mobile/utils/device';
 
 const AddShape = props => {
+    const storeShapeSettings = props.storeShapeSettings;
+    let shapes = storeShapeSettings.getStyleGroups();
     return (
-        <Fragment>
-
-        </Fragment>
+        <div className={'dataview shapes'}>
+            {shapes.map((row, indexRow) => {
+                return (
+                    <ul className="row" key={'shape-row-' + indexRow}>
+                        {row.map((shape, index) => {
+                            return (
+                                <li key={'shape-' + indexRow + '-' + index} onClick={() => {props.onShapeClick(shape.type)}}>
+                                    <div className="thumb"
+                                         style={{WebkitMaskImage: `url('resources/img/shapes/${shape.thumb}')`}}>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                )
+            })}
+        </div>
     )
 };
 
-export {AddShape};
+export default  inject("storeShapeSettings")(observer(AddShape));
