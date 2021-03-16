555e983bc3b4967acf4e873010ed1c3780881615
SergeyEzhin <ezhin@mail.ru>
2021-03-16 16:41:17 +0300

    [PE mobile] Small edits

--- vendor/framework7-react/babel.config.js
+++ vendor/framework7-react/babel.config.js
@@ -9,6 +9,6 @@ module.exports = {
     '@babel/plugin-transform-runtime',
     '@babel/plugin-syntax-dynamic-import',
     ['@babel/plugin-proposal-decorators', {'legacy': true }],
-    ['@babel/plugin-proposal-class-properties',{'loose':true}],
+    ['@babel/plugin-proposal-class-properties',{'loose': false}],
   ],
 };
