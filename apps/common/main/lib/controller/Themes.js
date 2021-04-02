bca6099e1868133db48e76ccf66745bb055aea5d
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2021-04-02 12:22:17 +0300

    [common] skip theme's colors updatre for IE

--- apps/common/main/lib/controller/Themes.js
+++ apps/common/main/lib/controller/Themes.js
@@ -18,22 +18,24 @@ define([
         }
 
         var refresh_sdk_colors = function () {
-            var style = getComputedStyle(document.body);
-            if ( !!window.DE ) {
-                var color_background_normal = style.getPropertyValue('--background-normal');
-                this.api.asc_setSkin({
-                    "RulerOutline": style.getPropertyValue('--border-toolbar'),
-                    "RulerMarkersFillColor": color_background_normal,
-                    "RulerMarkersFillColorOld": color_background_normal,
-                    "RulerTableColor1": color_background_normal,
-                    "RulerLight": style.getPropertyValue("--canvas-ruler-background"),
-                    "RulerDark": style.getPropertyValue("--canvas-ruler-margins-background"),
-                    "RulerTextColor": style.getPropertyValue("--canvas-ruler-mark"),
-                    "RulerTableColor2": style.getPropertyValue("--canvas-ruler-handle-border"),
-                    "RulerTableColor2Old": style.getPropertyValue("--canvas-ruler-handle-border-disabled"),
-                    "RulerTabsColor": style.getPropertyValue("--canvas-high-contrast"),
-                    "RulerTabsColorOld": style.getPropertyValue("--canvas-high-contrast-disabled"),
-                });
+            if ( !(Common.Utils.isIE10 || Common.Utils.isIE11) ) {
+                var style = getComputedStyle(document.body);
+                if ( !!window.DE ) {
+                    var color_background_normal = style.getPropertyValue('--background-normal');
+                    this.api.asc_setSkin({
+                        "RulerOutline": style.getPropertyValue('--border-toolbar'),
+                        "RulerMarkersFillColor": color_background_normal,
+                        "RulerMarkersFillColorOld": color_background_normal,
+                        "RulerTableColor1": color_background_normal,
+                        "RulerLight": style.getPropertyValue("--canvas-ruler-background"),
+                        "RulerDark": style.getPropertyValue("--canvas-ruler-margins-background"),
+                        "RulerTextColor": style.getPropertyValue("--canvas-ruler-mark"),
+                        "RulerTableColor2": style.getPropertyValue("--canvas-ruler-handle-border"),
+                        "RulerTableColor2Old": style.getPropertyValue("--canvas-ruler-handle-border-disabled"),
+                        "RulerTabsColor": style.getPropertyValue("--canvas-high-contrast"),
+                        "RulerTabsColorOld": style.getPropertyValue("--canvas-high-contrast-disabled"),
+                    });
+                }
             }
         }
         return {
