86b5673ce23961f646336697fb26d9b01c18d2de
ShimaginAndrey <andrey.shimagin@onlyoffice.com>
2021-04-19 12:07:13 +0300

    fix bug №48073

--- apps/presentationeditor/mobile/src/view/add/AddLink.jsx
+++ apps/presentationeditor/mobile/src/view/add/AddLink.jsx
@@ -1,19 +1,19 @@
-import React, {useState} from 'react';
-import {observer, inject} from "mobx-react";
-import {List, ListItem, Page, Navbar, Icon, ListButton, ListInput, Segmented, Button} from 'framework7-react';
+import React, { useState } from 'react';
+import { observer, inject } from "mobx-react";
+import { List, ListItem, Page, Navbar, Icon, ListButton, ListInput, Segmented, Button } from 'framework7-react';
 import { useTranslation } from 'react-i18next';
-import {Device} from "../../../../../common/mobile/utils/device";
+import { Device } from "../../../../../common/mobile/utils/device";
 
 const PageTypeLink = props => {
     const { t } = useTranslation();
-    const _t = t('View.Add', {returnObjects: true});
+    const _t = t('View.Add', { returnObjects: true });
     const [typeLink, setTypeLink] = useState(props.curType);
     return (
         <Page>
-            <Navbar title={_t.textLinkType} backLink={_t.textBack}/>
+            <Navbar title={_t.textLinkType} backLink={_t.textBack} />
             <List>
-                <ListItem title={_t.textExternalLink} radio checked={typeLink === 1} onClick={() => {setTypeLink(1); props.changeType(1);}}></ListItem>
-                <ListItem title={_t.textSlideInThisPresentation} radio checked={typeLink === 0} onClick={() => {setTypeLink(0); props.changeType(0);}}></ListItem>
+                <ListItem title={_t.textExternalLink} radio checked={typeLink === 1} onClick={() => { setTypeLink(1); props.changeType(1); }}></ListItem>
+                <ListItem title={_t.textSlideInThisPresentation} radio checked={typeLink === 0} onClick={() => { setTypeLink(0); props.changeType(0); }}></ListItem>
             </List>
         </Page>
     )
@@ -22,7 +22,7 @@ const PageTypeLink = props => {
 const PageLinkTo = props => {
     const isAndroid = Device.android;
     const { t } = useTranslation();
-    const _t = t('View.Add', {returnObjects: true});
+    const _t = t('View.Add', { returnObjects: true });
 
     const [stateTypeTo, setTypeTo] = useState(props.curTo);
     const changeTypeTo = (type) => {
@@ -44,26 +44,26 @@ const PageLinkTo = props => {
     };
     return (
         <Page>
-            <Navbar title={_t.textLinkTo} backLink={_t.textBack}/>
+            <Navbar title={_t.textLinkTo} backLink={_t.textBack} />
             <List>
-            <ListItem title={_t.textNextSlide} radio checked={stateTypeTo === 0} onClick={() => {changeTypeTo(0)}}></ListItem>
-            <ListItem title={_t.textPreviousSlide} radio checked={stateTypeTo === 1} onClick={() => {changeTypeTo(1)}}></ListItem>
-            <ListItem title={_t.textFirstSlide} radio checked={stateTypeTo === 2} onClick={() => {changeTypeTo(2)}}></ListItem>
-            <ListItem title={_t.textLastSlide} radio checked={stateTypeTo === 3} onClick={() => {changeTypeTo(3)}}></ListItem>
-            <ListItem title={_t.textSlideNumber}>
-                {!isAndroid && <div slot='after-start'>{stateNumberTo + 1}</div>}
-                <div slot='after'>
-                    <Segmented>
-                        <Button outline className='decrement item-link' onClick={() => {changeNumber(stateNumberTo, true);}}>
-                            {isAndroid ? <Icon icon="icon-expand-down"></Icon> : ' - '}
-                        </Button>
-                        {isAndroid && <label>{stateNumberTo + 1}</label>}
-                        <Button outline className='increment item-link' onClick={() => {changeNumber(stateNumberTo, false);}}>
-                            {isAndroid ? <Icon icon="icon-expand-up"></Icon> : ' + '}
-                        </Button>
-                    </Segmented>
-                </div>
-            </ListItem>
+                <ListItem title={_t.textNextSlide} radio checked={stateTypeTo === 0} onClick={() => { changeTypeTo(0) }}></ListItem>
+                <ListItem title={_t.textPreviousSlide} radio checked={stateTypeTo === 1} onClick={() => { changeTypeTo(1) }}></ListItem>
+                <ListItem title={_t.textFirstSlide} radio checked={stateTypeTo === 2} onClick={() => { changeTypeTo(2) }}></ListItem>
+                <ListItem title={_t.textLastSlide} radio checked={stateTypeTo === 3} onClick={() => { changeTypeTo(3) }}></ListItem>
+                <ListItem title={_t.textSlideNumber}>
+                    {!isAndroid && <div slot='after-start'>{stateNumberTo + 1}</div>}
+                    <div slot='after'>
+                        <Segmented>
+                            <Button outline className='decrement item-link' onClick={() => { changeNumber(stateNumberTo, true); }}>
+                                {isAndroid ? <Icon icon="icon-expand-down"></Icon> : ' - '}
+                            </Button>
+                            {isAndroid && <label>{stateNumberTo + 1}</label>}
+                            <Button outline className='increment item-link' onClick={() => { changeNumber(stateNumberTo, false); }}>
+                                {isAndroid ? <Icon icon="icon-expand-up"></Icon> : ' + '}
+                            </Button>
+                        </Segmented>
+                    </div>
+                </ListItem>
             </List>
         </Page>
     )
@@ -71,7 +71,8 @@ const PageLinkTo = props => {
 
 const PageLink = props => {
     const { t } = useTranslation();
-    const _t = t('View.Add', {returnObjects: true});
+    const _t = t('View.Add', { returnObjects: true });
+    const regx = /["https://"]/g
 
     const [typeLink, setTypeLink] = useState(1);
     const textType = typeLink === 1 ? _t.textExternalLink : _t.textSlideInThisPresentation;
@@ -87,11 +88,11 @@ const PageLink = props => {
     const changeTo = (type, number) => {
         setLinkTo(type);
         switch (type) {
-            case 0 : setDisplayTo(_t.textNextSlide); break;
-            case 1 : setDisplayTo(_t.textPreviousSlide); break;
-            case 2 : setDisplayTo(_t.textFirstSlide); break;
-            case 3 : setDisplayTo(_t.textLastSlide); break;
-            case 4 : setDisplayTo(`${_t.textSlide} ${number + 1}`); setNumberTo(number); break;
+            case 0: setDisplayTo(_t.textNextSlide); break;
+            case 1: setDisplayTo(_t.textPreviousSlide); break;
+            case 2: setDisplayTo(_t.textFirstSlide); break;
+            case 3: setDisplayTo(_t.textLastSlide); break;
+            case 4: setDisplayTo(`${_t.textSlide} ${number + 1}`); setNumberTo(number); break;
         }
     };
 
@@ -103,52 +104,59 @@ const PageLink = props => {
 
     return (
         <Page>
-            {!props.noNavbar && <Navbar title={_t.textLink} backLink={_t.textBack}/>}
+            {!props.noNavbar && <Navbar title={_t.textLink} backLink={_t.textBack} />}
             <List inlineLabels className='inputs-list'>
                 <ListItem link={'/add-link-type/'} title={_t.textLinkType} after={textType} routeProps={{
                     changeType: changeType,
                     curType: typeLink
-                }}/>
+                }} />
                 {typeLink === 1 ?
                     <ListInput label={_t.textLink}
-                               type="text"
-                               placeholder={_t.textLink}
-                               value={link}
-                               onChange={(event) => {setLink(event.target.value)}}
+                        type="text"
+                        placeholder={_t.textLink}
+                        value={link}
+                        onChange={(event) => {
+                            if (regx.test(link)) {
+                                setDisplay(event.target.value)
+                            }
+                            setLink(event.target.value)
+                        }}
                     /> :
                     <ListItem link={'/add-link-to/'} title={_t.textLinkTo} after={displayTo} routeProps={{
                         changeTo: changeTo,
                         curTo: linkTo
-                    }}/>
+                    }} />
                 }
                 <ListInput label={_t.textDisplay}
-                           type="text"
-                           placeholder={_t.textDisplay}
-                           value={stateDisplay}
-                           disabled={displayDisabled}
-                           onChange={(event) => {setDisplay(event.target.value)}}
+                    type="text"
+                    placeholder={_t.textDisplay}
+                    value={stateDisplay || link}
+                    disabled={displayDisabled}
+                    onChange={(event) => { setDisplay(event.target.value) }}
                 />
                 <ListInput label={_t.textScreenTip}
-                           type="text"
-                           placeholder={_t.textScreenTip}
-                           value={screenTip}
-                           onChange={(event) => {setScreenTip(event.target.value)}}
+                    type="text"
+                    placeholder={_t.textScreenTip}
+                    value={screenTip}
+                    onChange={(event) => { setScreenTip(event.target.value) }}
                 />
             </List>
             <List>
                 <ListButton title={_t.textInsert}
-                            className={`button-fill button-raised${typeLink === 1 && link.length < 1 && ' disabled'}`}
-                            onClick={() => {
-                                props.onInsertLink(typeLink, (typeLink === 1 ?
-                                    {url: link, display: stateDisplay, tip: screenTip, displayDisabled: displayDisabled } :
-                                    {linkTo: linkTo, numberTo: numberTo, display: stateDisplay, tip: screenTip, displayDisabled: displayDisabled}));
-                            }}
+                    className={`button-fill button-raised${typeLink === 1 && link.length < 1 && ' disabled'}`}
+                    onClick={() => {
+                        props.onInsertLink(typeLink, (typeLink === 1 ?
+                            { url: link, display: stateDisplay, tip: screenTip, displayDisabled: displayDisabled } :
+                            { linkTo: linkTo, numberTo: numberTo, display: stateDisplay, tip: screenTip, displayDisabled: displayDisabled }));
+                    }}
                 />
             </List>
         </Page>
     )
 };
 
-export {PageLink,
-        PageLinkTo,
-        PageTypeLink}
+export {
+    PageLink,
+    PageLinkTo,
+    PageTypeLink
+}
--- apps/spreadsheeteditor/mobile/src/view/add/AddLink.jsx
+++ apps/spreadsheeteditor/mobile/src/view/add/AddLink.jsx
@@ -1,41 +1,41 @@
-import React, {Fragment, useState} from 'react';
-import {Page, Navbar, BlockTitle, List, ListItem, ListInput, ListButton, Icon} from 'framework7-react';
+import React, { Fragment, useState } from 'react';
+import { Page, Navbar, BlockTitle, List, ListItem, ListInput, ListButton, Icon } from 'framework7-react';
 import { useTranslation } from 'react-i18next';
-import {Device} from "../../../../../common/mobile/utils/device";
+import { Device } from "../../../../../common/mobile/utils/device";
 
-const PageTypeLink = ({curType, changeType}) => {
+const PageTypeLink = ({ curType, changeType }) => {
     const { t } = useTranslation();
-    const _t = t('View.Add', {returnObjects: true});
+    const _t = t('View.Add', { returnObjects: true });
     const [typeLink, setTypeLink] = useState(curType);
     return (
         <Page>
-            <Navbar title={_t.textLinkType} backLink={_t.textBack}/>
+            <Navbar title={_t.textLinkType} backLink={_t.textBack} />
             <List>
-                <ListItem title={_t.textExternalLink} radio checked={typeLink === 'ext'} onClick={() => {setTypeLink('ext'); changeType('ext');}}></ListItem>
-                <ListItem title={_t.textInternalDataRange} radio checked={typeLink === 'int'} onClick={() => {setTypeLink('int'); changeType('int');}}></ListItem>
+                <ListItem title={_t.textExternalLink} radio checked={typeLink === 'ext'} onClick={() => { setTypeLink('ext'); changeType('ext'); }}></ListItem>
+                <ListItem title={_t.textInternalDataRange} radio checked={typeLink === 'int'} onClick={() => { setTypeLink('int'); changeType('int'); }}></ListItem>
             </List>
         </Page>
     )
 };
 
-const PageSheet = ({curSheet, sheets, changeSheet}) => {
+const PageSheet = ({ curSheet, sheets, changeSheet }) => {
     const { t } = useTranslation();
-    const _t = t('View.Add', {returnObjects: true});
+    const _t = t('View.Add', { returnObjects: true });
     const [stateSheet, setSheet] = useState(curSheet.value);
     return (
         <Page>
-            <Navbar title={_t.textSheet} backLink={_t.textBack}/>
+            <Navbar title={_t.textSheet} backLink={_t.textBack} />
             <List>
                 {sheets.map((sheet) => {
-                    return(
+                    return (
                         <ListItem key={`sheet-${sheet.value}`}
-                                  title={sheet.caption}
-                                  radio
-                                  checked={stateSheet === sheet.value}
-                                  onClick={() => {
-                                      setSheet(sheet.value);
-                                      changeSheet(sheet);
-                                  }}
+                            title={sheet.caption}
+                            radio
+                            checked={stateSheet === sheet.value}
+                            onClick={() => {
+                                setSheet(sheet.value);
+                                changeSheet(sheet);
+                            }}
                         />
                     )
                 })}
@@ -48,7 +48,8 @@ const PageSheet = ({curSheet, sheets, changeSheet}) => {
 const AddLinkView = props => {
     const isIos = Device.ios;
     const { t } = useTranslation();
-    const _t = t('View.Add', {returnObjects: true});
+    const _t = t('View.Add', { returnObjects: true });
+    const regx = /["https://"]/g
 
     const [typeLink, setTypeLink] = useState('ext');
     const textType = typeLink === 'ext' ? _t.textExternalLink : _t.textInternalDataRange;
@@ -80,15 +81,20 @@ const AddLinkView = props => {
                     <ListItem link={'/add-link-type/'} title={_t.textLinkType} after={textType} routeProps={{
                         changeType: changeType,
                         curType: typeLink
-                    }}/>
+                    }} />
                 }
                 {typeLink === 'ext' &&
                     <ListInput label={_t.textLink}
-                               type="text"
-                               placeholder={_t.textLink}
-                               value={link}
-                               onChange={(event) => {setLink(event.target.value)}}
-                               className={isIos ? 'list-input-right' : ''}
+                        type="text"
+                        placeholder={_t.textLink}
+                        value={link}
+                        onChange={(event) => {
+                            if (regx.test(link)) {
+                                setDisplayText(event.target.value)
+                            }
+                            setLink(event.target.value)
+                        }}
+                        className={isIos ? 'list-input-right' : ''}
                     />
                 }
                 {typeLink === 'int' &&
@@ -96,39 +102,41 @@ const AddLinkView = props => {
                         changeSheet: changeSheet,
                         sheets: props.sheets,
                         curSheet: curSheet
-                    }}/>
+                    }} />
                 }
                 {typeLink === 'int' &&
                     <ListInput label={_t.textRange}
-                               type="text"
-                               placeholder={_t.textRequired}
-                               value={range}
-                               onChange={(event) => {setRange(event.target.value)}}
-                               className={isIos ? 'list-input-right' : ''}
+                        type="text"
+                        placeholder={_t.textRequired}
+                        value={range}
+                        onChange={(event) => { setRange(event.target.value) }}
+                        className={isIos ? 'list-input-right' : ''}
                     />
                 }
                 <ListInput label={_t.textDisplay}
-                           type="text"
-                           placeholder={_t.textDisplay}
-                           value={stateDisplayText}
-                           disabled={displayDisabled}
-                           onChange={(event) => {setDisplayText(event.target.value)}}
-                           className={isIos ? 'list-input-right' : ''}
+                    type="text"
+                    placeholder={_t.textDisplay}
+                    value={stateDisplayText || link}
+                    disabled={displayDisabled}
+                    onChange={(event) => { setDisplayText(event.target.value) }}
+                    className={isIos ? 'list-input-right' : ''}
                 />
                 <ListInput label={_t.textScreenTip}
-                           type="text"
-                           placeholder={_t.textScreenTip}
-                           value={screenTip}
-                           onChange={(event) => {setScreenTip(event.target.value)}}
-                           className={isIos ? 'list-input-right' : ''}
+                    type="text"
+                    placeholder={_t.textScreenTip}
+                    value={screenTip}
+                    onChange={(event) => { setScreenTip(event.target.value) }}
+                    className={isIos ? 'list-input-right' : ''}
                 />
             </List>
             <List>
                 <ListButton title={_t.textInsert}
-                            className={`button-fill button-raised${(typeLink === 'ext' && link.length < 1 || typeLink === 'int' && range.length < 1) && ' disabled'}`}
-                            onClick={() => {props.onInsertLink(typeLink === 'ext' ?
-                                {type: 'ext', url: link, text: stateDisplayText, tooltip: screenTip} :
-                                {type: 'int', url: range, sheet: curSheet.caption, text: stateDisplayText, tooltip: screenTip})}}
+                    className={`button-fill button-raised${(typeLink === 'ext' && link.length < 1 || typeLink === 'int' && range.length < 1) && ' disabled'}`}
+                    onClick={() => {
+                        props.onInsertLink(typeLink === 'ext' ?
+                            { type: 'ext', url: link, text: stateDisplayText, tooltip: screenTip } :
+                            { type: 'int', url: range, sheet: curSheet.caption, text: stateDisplayText, tooltip: screenTip })
+                    }}
                 />
             </List>
         </Fragment>
@@ -137,15 +145,15 @@ const AddLinkView = props => {
 
 const AddLink = props => {
     const { t } = useTranslation();
-    const _t = t('View.Add', {returnObjects: true});
+    const _t = t('View.Add', { returnObjects: true });
     return (
         props.inTabs ?
-            <AddLinkView allowInternal={props.allowInternal} displayText={props.displayText} sheets={props.sheets} activeSheet={props.activeSheet} onInsertLink={props.onInsertLink}/> :
+            <AddLinkView allowInternal={props.allowInternal} displayText={props.displayText} sheets={props.sheets} activeSheet={props.activeSheet} onInsertLink={props.onInsertLink} /> :
             <Page>
-                <Navbar title={_t.textAddLink} backLink={_t.textBack}/>
-                <AddLinkView allowInternal={props.allowInternal} displayText={props.displayText} sheets={props.sheets} activeSheet={props.activeSheet} onInsertLink={props.onInsertLink}/>
+                <Navbar title={_t.textAddLink} backLink={_t.textBack} />
+                <AddLinkView allowInternal={props.allowInternal} displayText={props.displayText} sheets={props.sheets} activeSheet={props.activeSheet} onInsertLink={props.onInsertLink} />
             </Page>
     )
 };
 
-export {AddLink, PageTypeLink, PageSheet};
+export { AddLink, PageTypeLink, PageSheet };
