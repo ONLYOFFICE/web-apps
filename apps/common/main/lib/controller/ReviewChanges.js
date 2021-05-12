59c658ecab0067ad1abb8d61ad6bdba0ccdc90ae
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2021-05-13 00:57:01 +0300

    Wopi: change config parameters and post messages

--- apps/common/main/lib/controller/ReviewChanges.js
+++ apps/common/main/lib/controller/ReviewChanges.js
@@ -905,21 +905,17 @@ define([
             if (this.appConfig.canRequestSharingSettings) {
                 Common.Gateway.requestSharingSettings();
             } else {
-                if (this.appConfig.wopi) {
-                    window.open(this.appConfig.sharingSettingsUrl, "_blank");
-                } else {
-                    var me = this;
-                    me._docAccessDlg = new Common.Views.DocumentAccessDialog({
-                        settingsurl: this.appConfig.sharingSettingsUrl
-                    });
-                    me._docAccessDlg.on('accessrights', function(obj, rights){
-                        me.setSharingSettings({sharingSettings: rights});
-                    }).on('close', function(obj){
-                        me._docAccessDlg = undefined;
-                    });
+                var me = this;
+                me._docAccessDlg = new Common.Views.DocumentAccessDialog({
+                    settingsurl: this.appConfig.sharingSettingsUrl
+                });
+                me._docAccessDlg.on('accessrights', function(obj, rights){
+                    me.setSharingSettings({sharingSettings: rights});
+                }).on('close', function(obj){
+                    me._docAccessDlg = undefined;
+                });
 
-                    me._docAccessDlg.show();
-                }
+                me._docAccessDlg.show();
             }
         },
 
