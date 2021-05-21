44210f7a5aec28c73be8f68fe647fc846eedaeb5
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2021-05-21 11:19:26 +0300

    [all] prepare sprites for 1.25x and 1.75x scaling

--- build/sprites/Gruntfile.js
+++ build/sprites/Gruntfile.js
@@ -67,6 +67,25 @@ module.exports = function (grunt, rootpathprefix) {
                 scale: '2x',
                 extpath: 'huge'
             }),
+
+            'word1.25x': configTemplate({
+                editor:'documenteditor',
+                spritename: sprite_name,
+                scale: '1.25x'
+            }),
+            'word-big-1.25x': configTemplate({
+                editor:'documenteditor',
+                spritename: sprite_name_big,
+                scale: '1.25x',
+                extpath: 'big'
+            }),
+            'word-huge-1.25x': configTemplate({
+                editor:'documenteditor',
+                spritename: sprite_name_huge,
+                scale: '1.25x',
+                extpath: 'huge'
+            }),
+
             'word1.5x': configTemplate({
                 editor:'documenteditor',
                 spritename: sprite_name,
@@ -84,6 +103,25 @@ module.exports = function (grunt, rootpathprefix) {
                 scale: '1.5x',
                 extpath: 'huge'
             }),
+
+            'word1.75x': configTemplate({
+                editor:'documenteditor',
+                spritename: sprite_name,
+                scale: '1.75x'
+            }),
+            'word-big-1.75x': configTemplate({
+                editor:'documenteditor',
+                spritename: sprite_name_big,
+                scale: '1.75x',
+                extpath: 'big'
+            }),
+            'word-huge-1.75x': configTemplate({
+                editor:'documenteditor',
+                spritename: sprite_name_huge,
+                scale: '1.75x',
+                extpath: 'huge'
+            }),
+
             'slide-1x': configTemplate({
                 editor:'presentationeditor',
                 spritename: sprite_name,
@@ -157,7 +195,9 @@ module.exports = function (grunt, rootpathprefix) {
     grunt.loadNpmTasks('grunt-spritesmith');
 
     grunt.registerTask('word-icons', ['sprite:word-1x', 'sprite:word-big-1x', 'sprite:word-huge-1x', 'sprite:word-2x', 'sprite:word-big-2x', 'sprite:word-huge-2x',
-                                        'sprite:word1.5x', 'sprite:word-big-1.5x', 'sprite:word-huge-1.5x']);
+                                        'sprite:word1.25x', 'sprite:word-big-1.25x', 'sprite:word-huge-1.25x',
+                                        'sprite:word1.5x', 'sprite:word-big-1.5x', 'sprite:word-huge-1.5x',
+                                        'sprite:word1.75x', 'sprite:word-big-1.75x', 'sprite:word-huge-1.75x']);
     grunt.registerTask('slide-icons', ['sprite:slide-1x', 'sprite:slide-big-1x','sprite:slide-2x', 'sprite:slide-big-2x','sprite:slide-1.5x', 'sprite:slide-big-1.5x']);
     grunt.registerTask('cell-icons', ['sprite:cell-1x', 'sprite:cell-big-1x','sprite:cell-2x', 'sprite:cell-big-2x','sprite:cell-1.5x', 'sprite:cell-big-1.5x']);
 
