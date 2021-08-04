b16e82ea2eb7a7901678d0b650d2a919549233cb
ShimaginAndrey <andrey.shimagin@onlyoffice.com>
2021-08-04 18:09:50 +0300

    [SSE] Fix Bug 51707

--- apps/spreadsheeteditor/mobile/src/controller/CellEditor.jsx
+++ apps/spreadsheeteditor/mobile/src/controller/CellEditor.jsx
@@ -6,16 +6,23 @@ const CellEditor = props => {
     useEffect(() => {
         Common.Notifications.on('engineCreated', api => {
             api.asc_registerCallback('asc_onSelectionNameChanged', onApiCellSelection.bind(this));
+            api.asc_registerCallback('asc_onSelectionChanged', onApiSelectionChanged.bind(this));
         });
     }, []);
 
     const [cellName, setCellName] = useState('');
+    const [stateCoauth, setCoauthDisabled] = useState(null);
 
     const onApiCellSelection = info => {
         setCellName(typeof(info)=='string' ? info : info.asc_getName());
     };
 
+    const onApiSelectionChanged = info => {
+        setCoauthDisabled(info.asc_getLocked() === true || info.asc_getLockedTable() === true || info.asc_getLockedPivotTable()===true);
+    }
+    
     return <CellEditorView cellName={cellName}
+                           stateCoauth = {stateCoauth}
                            onClickToOpenAddOptions={props.onClickToOpenAddOptions}/>
 };
 
