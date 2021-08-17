d9757ca68e223b1f2e120ae1a45723cd5577b618
JuliaSvinareva <Julia.Svinaryova@onlyoffice.com>
2021-08-17 16:55:46 +0300

    Fix visibility of alt-key hints when we need to scroll tabs, add hints to scroll buttons

--- apps/common/main/lib/component/HintManager.js
+++ apps/common/main/lib/component/HintManager.js
@@ -260,6 +260,8 @@ Common.UI.HintManager = new(function() {
             _getControls();
         _currentControls.forEach(function(item, index) {
             if (!_isItemDisabled(item)) {
+                var leftBorder = 0,
+                    rightBorder = docW;
                 if ($(_currentSection).prop('id') === 'toolbar' && ($(_currentSection).find('.toolbar-mask').length > 0 || item.closest('.group').find('.toolbar-group-mask').length > 0)) {
                     return;
                 }
@@ -267,6 +269,15 @@ Common.UI.HintManager = new(function() {
                     var $statusbar = item.parent();
                     if (item.offset().left > $statusbar.offset().left + $statusbar.width()) {
                         return;
+                    }
+                }
+                if (_currentLevel === 0 && item.closest('.tabs.short').length > 0) {
+                    var blockTabs = item.closest('.tabs.short');
+                    leftBorder = blockTabs.offset().left;
+                    rightBorder = leftBorder + blockTabs.width();
+                    if (!item.hasClass('scroll')) {
+                        leftBorder += 20;
+                        rightBorder -= 20;
                     }
                 }
                 var hint = $('<div style="" class="hint-div">' + item.attr('data-hint-title') + '</div>');
@@ -327,7 +338,7 @@ Common.UI.HintManager = new(function() {
                     left = offset.left + (item.outerWidth() - 18) / 2 + offsets[1];
                 }
 
-                if (top < maxHeight && left < docW && top > topSection && top < bottomSection) {
+                if (top < maxHeight && left < docW && top > topSection && top < bottomSection && left > leftBorder && left + 18 < rightBorder) {
                     hint.css({
                         top: top,
                         left: left
@@ -452,7 +463,7 @@ Common.UI.HintManager = new(function() {
                                         }
                                     }
                                 }
-                                if (curr.prop('id') === 'btn-goback' || curr.closest('.btn-slot').prop('id') === 'slot-btn-options' || curr.prop('id') === 'left-btn-thumbs') {
+                                if (curr.prop('id') === 'btn-goback' || curr.closest('.btn-slot').prop('id') === 'slot-btn-options' || curr.prop('id') === 'left-btn-thumbs' || curr.hasClass('scroll')) {
                                     _resetToDefault();
                                     return;
                                 }
