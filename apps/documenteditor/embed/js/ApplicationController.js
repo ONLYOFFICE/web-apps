b6ee27af277ecafdd199d911701827e31ba07b7f
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2021-08-18 11:58:45 +0300

    [DE embedded] Fix Bug 51995

--- apps/documenteditor/embed/js/ApplicationController.js
+++ apps/documenteditor/embed/js/ApplicationController.js
@@ -514,7 +514,6 @@ DE.ApplicationController = new(function(){
             $('#id-btn-clear-fields').hide();
             btnSubmit.hide();
         } else {
-            $('#id-pages').hide();
             $('#id-btn-next-field .caption').text(me.textNext);
             $('#id-btn-clear-fields .caption').text(me.textClear);
 
