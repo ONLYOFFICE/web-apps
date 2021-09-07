a7dad54f7b4ea692e09d508837af9bd50290295e
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2021-09-07 12:42:24 +0300

    Don't sort fonts

--- apps/common/main/lib/collection/Fonts.js
+++ apps/common/main/lib/collection/Fonts.js
@@ -48,12 +48,6 @@ define([
     'common/main/lib/model/Font'
 ], function(Backbone){ 'use strict';
     Common.Collections.Fonts = Backbone.Collection.extend({
-        model: Common.Models.Font,
-        comparator: function(item1, item2) {
-            var n1 = item1.get('name').toLowerCase(),
-                n2 = item2.get('name').toLowerCase();
-            if (n1==n2) return 0;
-            return (n1<n2) ? -1 : 1;
-        }
+        model: Common.Models.Font
     });
 });
