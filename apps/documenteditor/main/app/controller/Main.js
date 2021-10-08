bb417b06780197a44ee10d1ebbd9f01f5be2ae04
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2021-10-08 20:14:28 +0300

    [DE] Download as oform and docxf

--- apps/documenteditor/main/app/controller/Main.js
+++ apps/documenteditor/main/app/controller/Main.js
@@ -545,7 +545,9 @@ define([
                             Asc.c_oAscFileType.OTT,
                             Asc.c_oAscFileType.FB2,
                             Asc.c_oAscFileType.EPUB,
-                            Asc.c_oAscFileType.DOCM
+                            Asc.c_oAscFileType.DOCM,
+                            Asc.c_oAscFileType.DOCXF,
+                            Asc.c_oAscFileType.OFORM
                         ];
 
                     if ( !_format || _supported.indexOf(_format) < 0 )
