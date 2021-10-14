a7a924ce28b882496ebc5fb62ea19a98222c9b24
ShimaginAndrey <andrey.shimagin@onlyoffice.com>
2021-10-14 16:44:53 +0300

    [SSE] Fix Bug 53149

--- apps/spreadsheeteditor/mobile/src/controller/ContextMenu.jsx
+++ apps/spreadsheeteditor/mobile/src/controller/ContextMenu.jsx
@@ -26,6 +26,7 @@ class ContextMenu extends ContextMenuController {
         this.onApiShowComment = this.onApiShowComment.bind(this);
         this.onApiHideComment = this.onApiHideComment.bind(this);
         this.getUserName = this.getUserName.bind(this);
+        this.onApiHyperlinkClick = this.onApiHyperlinkClick.bind(this);
     }
 
     static closeContextMenu() {
@@ -43,6 +44,7 @@ class ContextMenu extends ContextMenuController {
         const api = Common.EditorApi.get();
         api.asc_unregisterCallback('asc_onShowComment', this.onApiShowComment);
         api.asc_unregisterCallback('asc_onHideComment', this.onApiHideComment);
+        api.asc_unregisterCallback('asc_onHyperlinkClick', this.onApiHyperlinkClick);
     }
 
 
@@ -57,6 +59,20 @@ class ContextMenu extends ContextMenuController {
     // onMenuClosed() {
     //     super.onMenuClosed();
     // }
+
+    onApiHyperlinkClick(url) {
+        const { t } = this.props;
+
+        if(!url) {
+            f7.dialog.create({
+                title: t('ContextMenu.notcriticalErrorTitle'),
+                text: t('ContextMenu.errorInvalidLink'),
+                buttons:[
+                    {text: 'OK'}
+                ] 
+            }).open();
+        }
+    }
 
     onMenuItemClick(action) {
         const { t } = this.props;
@@ -175,6 +191,7 @@ class ContextMenu extends ContextMenuController {
         const api = Common.EditorApi.get();
         api.asc_registerCallback('asc_onShowComment', this.onApiShowComment);
         api.asc_registerCallback('asc_onHideComment', this.onApiHideComment);
+        api.asc_registerCallback('asc_onHyperlinkClick', this.onApiHyperlinkClick);
     }
 
     initMenuItems() {
