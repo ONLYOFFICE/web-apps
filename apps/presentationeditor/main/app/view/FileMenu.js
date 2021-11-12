309f838aeaa00fa8e52890f0272e215bdba707ad
JuliaSvinareva <Julia.Svinaryova@onlyoffice.com>
2021-11-12 18:54:49 +0300

    [DE PE SSE] Fix alt hints in file menu

--- apps/presentationeditor/main/app/view/FileMenu.js
+++ apps/presentationeditor/main/app/view/FileMenu.js
@@ -95,7 +95,8 @@ define([
                 disabled: true,
                 dataHint: 1,
                 dataHintDirection: 'left-top',
-                dataHintOffset: [2, 14]
+                dataHintOffset: [2, 14],
+                dataHintTitle: 'S'
             });
             if ( !!this.options.miSave ) {
                 this.miSave.setDisabled(this.options.miSave.isDisabled());
@@ -149,7 +150,8 @@ define([
                 canFocused: false,
                 dataHint: 1,
                 dataHintDirection: 'left-top',
-                dataHintOffset: [2, 14]
+                dataHintOffset: [2, 14],
+                dataHintTitle: 'P'
             });
 
             this.miRename = new Common.UI.MenuItem({
