13943632cbbbb9f762b720ea9f27dc530424d58e
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2021-11-13 13:55:56 +0300

    [common] fixed lang loading

--- apps/common/locale.js
+++ apps/common/locale.js
@@ -127,7 +127,7 @@ Common.Locale = new(function() {
                 l10n = json || {};
                 apply && _applyLocalization();
             }).catch(function(e) {
-                if ( currentLang != defLang ) {
+                if ( currentLang != defLang && defLang && defLang.length < 3 ) {
                     return setTimeout(function(){
                         _requireLang(defLang)
                     }, 0);
