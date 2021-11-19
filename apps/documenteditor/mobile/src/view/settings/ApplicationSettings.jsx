739cf52862c0cbcf2d1bb4a80651226b96d954d2
ShimaginAndrey <andrey.shimagin@onlyoffice.com>
2021-11-19 16:00:00 +0300

    [DE] Added Themes controller and correct styles

--- apps/documenteditor/mobile/src/view/settings/ApplicationSettings.jsx
+++ apps/documenteditor/mobile/src/view/settings/ApplicationSettings.jsx
@@ -2,6 +2,7 @@ import React, {Fragment, useState} from "react";
 import { observer, inject } from "mobx-react";
 import { Page, Navbar, List, ListItem, BlockTitle, Toggle } from "framework7-react";
 import { useTranslation } from "react-i18next";
+import { Themes } from '../../../../../common/mobile/lib/controller/Themes.js';
 
 const PageApplicationSettings = props => {
     const { t } = useTranslation();
@@ -14,7 +15,7 @@ const PageApplicationSettings = props => {
     const isHiddenTableBorders = store.isHiddenTableBorders;
     const isComments = store.isComments;
     const isResolvedComments = store.isResolvedComments;
-    const [isThemeDark, setIsThemeDark] = useState(props.isThemeDark);
+    const [isThemeDark, setIsThemeDark] = useState(Themes.isCurrentDark);
 
     const changeMeasureSettings = value => {
         store.changeUnitMeasurement(value);
@@ -51,7 +52,7 @@ const PageApplicationSettings = props => {
                         </ListItem>
                         <ListItem title={'Dark theme'}>
                             <Toggle checked={isThemeDark}
-                                onToggleChange={toggle => {props.switchDarkTheme(!toggle), setIsThemeDark(!toggle)}}>
+                                onToggleChange={toggle => {Themes.switchDarkTheme(!toggle), setIsThemeDark(!toggle)}}>
                             </Toggle>
                         </ListItem>
                     </List>
