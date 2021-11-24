31aed3acae30d83c2f0f8055da416d2d48e86e5a
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2021-11-24 10:53:00 +0300

    Fix Bug 54038

--- apps/spreadsheeteditor/main/app/view/ShapeSettings.js
+++ apps/spreadsheeteditor/main/app/view/ShapeSettings.js
@@ -1124,7 +1124,7 @@ define([
 
                     this._state.GradColor = color;
                 }
-
+                this.chShadow.setDisabled(!!shapeprops.get_FromChart());
                 this.chShadow.setValue(!!shapeprops.asc_getShadow(), true);
 
                 this._noApply = false;
