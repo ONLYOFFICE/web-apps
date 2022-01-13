0cc527562522fc9c9085af4f62bec6907620611b
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2022-01-13 22:38:44 +0300

    [DE] Fix comment lock

--- apps/documenteditor/main/app/controller/Toolbar.js
+++ apps/documenteditor/main/app/controller/Toolbar.js
@@ -3213,7 +3213,8 @@ define([
             if ( config.canCoAuthoring && config.canComments ) {
                 this.btnsComment = Common.Utils.injectButtons(this.toolbar.$el.find('.slot-comment'), 'tlbtn-addcomment-', 'toolbar__icon btn-menu-comments', this.toolbar.capBtnComment,
                             [  Common.enumLock.paragraphLock, Common.enumLock.headerLock, Common.enumLock.richEditLock, Common.enumLock.plainEditLock, Common.enumLock.richDelLock, Common.enumLock.plainDelLock,
-                                    Common.enumLock.cantAddQuotedComment, Common.enumLock.imageLock, Common.enumLock.inSpecificForm, Common.enumLock.inImage, Common.enumLock.lostConnect, Common.enumLock.disableOnStart],
+                                    Common.enumLock.cantAddQuotedComment, Common.enumLock.imageLock, Common.enumLock.inSpecificForm, Common.enumLock.inImage, Common.enumLock.lostConnect, Common.enumLock.disableOnStart,
+                                    Common.enumLock.previewReviewMode, Common.enumLock.viewFormMode ],
                                  undefined, undefined, undefined, '1', 'bottom');
                 if ( this.btnsComment.length ) {
                     var _comments = DE.getController('Common.Controllers.Comments').getView();
@@ -3227,6 +3228,7 @@ define([
                     }, this);
                 }
                 Array.prototype.push.apply(this.toolbar.paragraphControls, this.btnsComment);
+                Array.prototype.push.apply(this.toolbar.lockControls, this.btnsComment);
             }
 
             (new Promise(function(accept) {
