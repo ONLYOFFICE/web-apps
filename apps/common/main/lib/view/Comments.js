c89170ce299d84864e46427b440273ca9dea44e1
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2022-02-01 18:18:03 +0300

    Fix Bug 52904

--- apps/common/main/lib/view/Comments.js
+++ apps/common/main/lib/view/Comments.js
@@ -806,7 +806,7 @@ define([
         },
 
         pickEMail: function (commentId, message) {
-            var arr = Common.Utils.String.htmlEncode(message).match(/\B[@+][A-Z0-9._%+-]+@[A-Z0-9._]+\.[A-Z]+\b/gi);
+            var arr = Common.Utils.String.htmlEncode(message).match(/\B[@+][A-Z0-9._%+-]+@[A-Z0-9._-]+\.[A-Z]+\b/gi);
             arr = _.map(arr, function(str){
                 return str.slice(1, str.length);
             });
