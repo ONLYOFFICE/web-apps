4e91fb42a3dd7ffddd8cd41fb8f6bb9a3253c60d
JuliaSvinareva <Julia.Svinaryova@onlyoffice.com>
2022-07-22 15:35:24 +0300

    [DE PE SSE] Bug 55070

--- apps/spreadsheeteditor/main/app/view/FileMenuPanels.js
+++ apps/spreadsheeteditor/main/app/view/FileMenuPanels.js
@@ -1465,7 +1465,7 @@ SSE.Views.FileMenuPanels.RecentFiles = Common.UI.BaseView.extend({
             this.lblApplication = $markup.findById('#id-info-appname');
             this.tblAuthor = $markup.findById('#id-info-author table');
             this.trAuthor = $markup.findById('#id-info-add-author').closest('tr');
-            this.authorTpl = '<tr><td><div style="display: inline-block;width: 200px;"><input type="text" spellcheck="false" class="form-control" readonly="true" value="{0}" ></div><div class="tool close img-commonctrl" data-hint="2" data-hint-direction="right" data-hint-offset="small"></div></td></tr>';
+            this.authorTpl = '<tr><td><div style="display: inline-block;width: 200px;"><input type="text" spellcheck="false" class="form-control" readonly="true" value="{0}" ></div><div class="tool close img-commonctrl img-colored" data-hint="2" data-hint-direction="right" data-hint-offset="small"></div></td></tr>';
 
             this.tblAuthor.on('click', function(e) {
                 var btn = $markup.find(e.target);
