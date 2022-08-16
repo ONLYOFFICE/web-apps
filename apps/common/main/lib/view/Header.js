9e3da1a47ee3647e7de3b19a766707cf08d661f5
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2022-08-17 02:01:16 +0300

    Fix file renaming in Firefox (text selection) + Bug 58378

--- apps/common/main/lib/view/Header.js
+++ apps/common/main/lib/view/Header.js
@@ -756,7 +756,7 @@ define([
                             'focus': onFocusDocName.bind(this),
                             'blur': function (e) {
                                 me.imgCrypted && me.imgCrypted.toggleClass('hidden', false);
-                                //label[0].selectionStart = label[0].selectionEnd = 0;
+                                Common.Utils.isGecko && (label[0].selectionStart = label[0].selectionEnd = 0);
                                 if(!me.isSaveDocName) {
                                     me.withoutExt = false;
                                     me.setDocTitle(me.documentCaption);
