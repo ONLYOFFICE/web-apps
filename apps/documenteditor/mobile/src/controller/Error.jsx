3dedbaf786598c986eccc67a2832a42fe385eb87
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2022-09-08 15:54:40 +0300

    [Mobile] Bug 58827

--- apps/documenteditor/mobile/src/controller/Error.jsx
+++ apps/documenteditor/mobile/src/controller/Error.jsx
@@ -189,6 +189,10 @@ const ErrorController = inject('storeAppOptions')(({storeAppOptions, LoadingDocu
                 config.msg = _t.errorNoTOC;
                 break;
 
+            case Asc.c_oAscError.ID.TextFormWrongFormat:
+                config.msg = _t.errorTextFormWrongFormat;
+                break;
+
             default:
                 config.msg = _t.errorDefaultMessage.replace('%1', id);
                 break;
