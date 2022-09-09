bf4b5faddb41a5aa15e718456dc8358b83d0218d
SergeyEzhin <ezhin@mail.ru>
2022-09-09 16:29:32 +0300

    [SSE mobile] Fix Bug 57730

--- apps/spreadsheeteditor/mobile/src/view/add/AddChart.jsx
+++ apps/spreadsheeteditor/mobile/src/view/add/AddChart.jsx
@@ -1,38 +1,65 @@
 import React, {Fragment, useState} from 'react';
 import {observer, inject} from "mobx-react";
 import {Swiper, SwiperSlide} from 'framework7-react';
+import {Device} from "../../../../../common/mobile/utils/device";
 
 const AddChart = props => {
     const types = props.storeChartSettings.types;
     const countSlides = Math.floor(types.length / 3);
-    const arraySlides = Array(countSlides).fill(countSlides);
+    const arraySlides = !Device.phone ? Array(countSlides).fill(countSlides) : [types.slice(0, 6), types.slice(6)];
     
     return (
         <div className={'dataview chart-types'}>
             {types && types.length ? (
                 <Swiper pagination={true}>
-                    {arraySlides.map((_, indexSlide) => {
-                        let typesSlide = types.slice(indexSlide * 3, (indexSlide * 3) + 3);
+                    {Device.phone ?
+                        arraySlides.map((typesSlide, indexSlide) => {
+                            return (
+                                <SwiperSlide key={indexSlide}>
+                                    {typesSlide.map((row, indexRow) => {
+                                        return (
+                                            <ul className="row" key={`row-${indexRow}`}>
+                                                {row.map((type, index) => {
+                                                    return (
+                                                        <li key={`${indexRow}-${index}`}
+                                                            onClick={() => {
+                                                                props.onInsertChart(type.type)
+                                                            }}>
+                                                            <div className={`thumb ${type.thumb}`}></div>
+                                                        </li>
+                                                    )
+                                                })}
+                                            </ul>
+                                        )
+                                    })}
+                                </SwiperSlide>
+                            )
+                        })
+                        :
+                        arraySlides.map((_, indexSlide) => {
+                            let typesSlide = types.slice(indexSlide * 3, (indexSlide * 3) + 3);
 
-                        return (
-                            <SwiperSlide key={indexSlide}>
-                                {typesSlide.map((row, indexRow) => {
-                                    return (
-                                        <ul className="row" key={`row-${indexRow}`}>
-                                            {row.map((type, index) => {
-                                                return (
-                                                    <li key={`${indexRow}-${index}`}
-                                                        onClick={()=>{props.onInsertChart(type.type)}}>
-                                                        <div className={`thumb ${type.thumb}`}></div>
-                                                    </li>
-                                                )
-                                            })}
-                                        </ul>
-                                    )
-                                })}
-                            </SwiperSlide>
-                        )
-                    })}
+                            return (
+                                <SwiperSlide key={indexSlide}>
+                                    {typesSlide.map((row, indexRow) => {
+                                        return (
+                                            <ul className="row" key={`row-${indexRow}`}>
+                                                {row.map((type, index) => {
+                                                    return (
+                                                        <li key={`${indexRow}-${index}`}
+                                                            onClick={() => {
+                                                                props.onInsertChart(type.type)
+                                                            }}>
+                                                            <div className={`thumb ${type.thumb}`}></div>
+                                                        </li>
+                                                    )
+                                                })}
+                                            </ul>
+                                        )
+                                    })}
+                                </SwiperSlide>
+                            )
+                        })}
                 </Swiper>
             ) : null}
         </div>
