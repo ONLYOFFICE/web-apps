20fa8d11c1f70c6bc200d721eb569aac904def0f
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2022-10-10 22:34:05 +0300

    Fix typo

--- apps/documenteditor/forms/app/controller/ApplicationController.js
+++ apps/documenteditor/forms/app/controller/ApplicationController.js
@@ -364,9 +364,10 @@ define([
         },
 
         onCountPages: function(count) {
-            maxPages = count;
-            if (maxPages !== count)
+            if (maxPages !== count) {
+                maxPages = count;
                 $('#pages').text(this.textOf + " " + count);
+            }
         },
 
         onCurrentPage: function(number) {
