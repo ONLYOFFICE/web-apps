3422e014d1e50e7c44634c50d189d54351f7571d
SergeyEzhin <ezhin@mail.ru>
2022-12-01 17:39:06 +0400

    [DE PE SSE mobile] Fix Bug 59928

--- apps/common/mobile/lib/view/SharingSettings.jsx
+++ apps/common/mobile/lib/view/SharingSettings.jsx
@@ -1,18 +1,15 @@
-import React, { Component, useEffect } from 'react';
-import { observer, inject } from "mobx-react";
-import { f7, Popover, List, ListItem, Navbar, NavRight, Sheet, BlockTitle, Page, View, Icon, Link } from 'framework7-react';
+import React from 'react';
+import { Navbar, Page } from 'framework7-react';
 import { useTranslation } from 'react-i18next';
-import { Device } from "../../utils/device";
 
-const SharingSettings = props => {
+const ViewSharingSettings = props => {
     const { t } = useTranslation();
-    const storeAppOptions = props.storeAppOptions;
-    const sharingSettingsUrl = storeAppOptions.sharingSettingsUrl;
+    const sharingSettingsUrl = props.sharingSettingsUrl;
     const _t = t('Common.Collaboration', {returnObjects: true});
 
     function resizeHeightIframe(iFrame) {
         iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
-    }
+    };
 
     return (
         <Page>
@@ -22,6 +19,6 @@ const SharingSettings = props => {
             </div>
         </Page>
     )
-}
+};
 
-export default inject("storeAppOptions")(observer(SharingSettings));
+export default ViewSharingSettings;
