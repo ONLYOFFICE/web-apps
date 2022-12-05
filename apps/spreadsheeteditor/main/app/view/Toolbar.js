fad08ff7e723af01a7ac772d19949e4facc2a20e
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2022-12-05 12:05:43 +0300

    [SSE] Fix internal editors

--- apps/spreadsheeteditor/main/app/view/Toolbar.js
+++ apps/spreadsheeteditor/main/app/view/Toolbar.js
@@ -2317,7 +2317,7 @@ define([
                                 [Common.enumLock.editCell, Common.enumLock.selRangeEdit, Common.enumLock.headerLock, Common.enumLock.lostConnect, Common.enumLock.coAuth], undefined, undefined, undefined, '1', 'bottom', 'small');
             Array.prototype.push.apply(this.lockControls, this.btnsEditHeader);
 
-            this.btnPrint.menu && this.btnPrint.$el.addClass('split');
+            this.btnPrint && this.btnPrint.menu && this.btnPrint.$el.addClass('split');
             return $host;
         },
 
