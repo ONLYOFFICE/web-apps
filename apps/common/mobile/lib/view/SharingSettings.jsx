8c7a5bafef248936fb7f0b3a3324f26a05652d0c
SergeyEzhin <ezhin@mail.ru>
2022-12-09 16:04:33 +0400

    [DE PE SSE mobile] Fix Bug 59914

--- apps/common/mobile/lib/view/SharingSettings.jsx
+++ apps/common/mobile/lib/view/SharingSettings.jsx
@@ -7,20 +7,11 @@ const ViewSharingSettings = props => {
     const sharingSettingsUrl = props.sharingSettingsUrl;
     const _t = t('Common.Collaboration', {returnObjects: true});
 
-    function resizeHeightIframe(selector) {
-        const iFrame = document.querySelector(selector);
-        iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
-    };
-
-    useEffect(() => {
-        resizeHeightIframe('#sharing-placeholder iframe');
-    }, []);
-
     return (
         <Page>
             <Navbar title={t('Common.Collaboration.textSharingSettings')} backLink={_t.textBack} />
             <div id="sharing-placeholder" className="sharing-placeholder">
-                <iframe width="100%" frameBorder={0} scrolling="0" align="top" src={sharingSettingsUrl}></iframe>
+                <iframe width="100%" height="500" frameBorder={0} scrolling="0" align="top" src={sharingSettingsUrl}></iframe>
             </div>
         </Page>
     )
