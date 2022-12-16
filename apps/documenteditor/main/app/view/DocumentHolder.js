4a97fcd4d1309914545749bd43c57fe0fb056fbc
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2022-12-16 18:21:57 +0300

    Fix signatures for protected mode

--- apps/documenteditor/main/app/view/DocumentHolder.js
+++ apps/documenteditor/main/app/view/DocumentHolder.js
@@ -2931,7 +2931,7 @@ define([
 
         SetDisabled: function(state, canProtect, fillFormMode) {
             this._isDisabled = state;
-            this._canProtect = canProtect;
+            this._canProtect =  state ? canProtect : true;
             this._fillFormMode = state ? fillFormMode : false;
         },
 
