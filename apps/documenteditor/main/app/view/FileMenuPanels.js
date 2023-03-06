23000685b9ec8123fd3765b7dd051cfe0537149c
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2023-03-06 19:32:07 +0300

    [DE][PE] Fix Bug 61510

--- apps/documenteditor/main/app/view/FileMenuPanels.js
+++ apps/documenteditor/main/app/view/FileMenuPanels.js
@@ -2420,7 +2420,7 @@ define([
                                 '<tr class="fms-btn-apply"><td>',
                                     '<div class="footer justify">',
                                         '<button id="print-btn-print" class="btn normal dlg-btn primary" result="print" style="width: 96px;" data-hint="2" data-hint-direction="bottom" data-hint-offset="big"><%= scope.txtPrint %></button>',
-                                        '<button id="print-btn-print-pdf" class="btn normal dlg-btn" result="pdf" style="width: 96px;" data-hint="2" data-hint-direction="bottom" data-hint-offset="big"><%= scope.txtPrintPdf %></button>',
+                                        '<button id="print-btn-print-pdf" class="btn normal dlg-btn" result="pdf" style="min-width: 96px;width: auto;" data-hint="2" data-hint-direction="bottom" data-hint-offset="big"><%= scope.txtPrintPdf %></button>',
                                     '</div>',
                                 '</td></tr>',
                             '</tbody>',
