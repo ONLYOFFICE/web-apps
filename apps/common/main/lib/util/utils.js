165ffa812d820d6836b6092aa860b06792e67849
Alexei Koshelev <aleksei.koshelev@onlyoffice.com>
2023-06-14 00:20:54 +0300

    Added getUserInitials utils

--- apps/common/main/lib/util/utils.js
+++ apps/common/main/lib/util/utils.js
@@ -1176,6 +1176,18 @@ Common.Utils.UserInfoParser = new(function() {
     }
 })();
 
+Common.Utils.getUserInitials = function(username) {
+    var fio = username.split(' ');
+    var initials = fio[0].substring(0, 1).toUpperCase();
+    for (var i = fio.length-1; i>0; i--) {
+        if (fio[i][0]!=='(' && fio[i][0]!==')') {
+            initials += fio[i].substring(0, 1).toUpperCase();
+            break;
+        }
+    }
+    return initials;
+};
+
 Common.Utils.getKeyByValue = function(obj, value) {
     for(var prop in obj) {
         if(obj.hasOwnProperty(prop)) {
