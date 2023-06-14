edefbc4ef5918c6946f1e28d696f9de5be8c8815
SergeyEzhin <ezhin@mail.ru>
2023-06-14 23:48:48 +0300

    [DE mobile] Add version history view

--- apps/common/mobile/lib/view/VersionHistory.jsx
+++ apps/common/mobile/lib/view/VersionHistory.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Page, Navbar } from "framework7-react";
+import React, { Fragment } from 'react';
+import { Page, Navbar, BlockTitle, List, ListItem, Icon } from "framework7-react";
 import { useTranslation } from "react-i18next";
 import { observer, inject } from "mobx-react";
 
@@ -10,8 +10,20 @@ const VersionHistoryView = inject('storeVersionHistory')(observer(props => {
     const _t = t("Settings", { returnObjects: true });
 
     return (
-        <Page>
+        <Page className='page-version-history'>
             <Navbar title={t('Settings.textVersionHistory')} backLink={_t.textBack} />
+            {arrVersions.map((version, index) => {
+                return (
+                    <div className='version-history-block' key={index} onClick={() => props.onSelectRevision(version)}>
+                        <BlockTitle className='version-history-block__title'>{`${t('Settings.textVersion')} ${version.revision}`}</BlockTitle>
+                        <List className='version-history-block__list' dividersIos mediaList outlineIos strongIos>
+                            <ListItem link='#' title={version.created} subtitle={version.username} >
+                                <Icon slot='media' icon='icon-doc-history' />
+                            </ListItem>
+                        </List>
+                    </div>
+                )
+            })}
         </Page>
     )
 }));
