bb2ec7518d7ca1bbf6ab03b410412911048b7c69
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2023-07-20 17:10:28 +0300

    Use pdf api

--- apps/pdfeditor/main/app/controller/Viewport.js
+++ apps/pdfeditor/main/app/controller/Viewport.js
@@ -131,7 +131,7 @@ define([
             // Create and render main view
             this.viewport = this.createView('Viewport').render();
 
-            this.api = new Asc.asc_docs_api({
+            this.api = new Asc.PDFEditorApi({
                 'id-view'  : 'editor_sdk',
                 'translate': this.getApplication().getController('Main').translationTable
             });
