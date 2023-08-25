7f15dedb857d9990ff8f477ad59405930ac2cba3
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2023-08-25 19:51:46 +0300

    [DE] Add hyphenation zone option

--- apps/documenteditor/main/app/view/HyphenationDialog.js
+++ apps/documenteditor/main/app/view/HyphenationDialog.js
@@ -61,10 +61,34 @@ define([
 
             this.template = [
                 '<div class="box" style="">',
-                    '<div id="hyphenation-chk-auto" style="margin-bottom: 15px;"></div>',
-                    '<div id="hyphenation-chk-caps" style="margin-bottom: 15px;"></div>',
-                    '<label class="margin-right-5">' + this.textLimit + '</label>',
-                    '<div id="hyphenation-num-limit" style="margin-bottom: 10px;display: inline-block;"></div>',
+                    '<table cols="2" style="width: 100%;">',
+                        '<tr>',
+                            '<td colspan="2" style="padding-bottom: 15px;">',
+                                '<div id="hyphenation-chk-auto"></div>',
+                            '</td>',
+                        '</tr>',
+                        '<tr>',
+                            '<td colspan="2" style="padding-bottom: 15px;">',
+                                '<div id="hyphenation-chk-caps"></div>',
+                            '</td>',
+                        '</tr>',
+                        '<tr>',
+                            '<td style="padding-bottom: 10px;">',
+                                '<label class="margin-right-5">' + this.textZone + '</label>',
+                            '</td>',
+                            '<td style="padding-bottom: 10px;">',
+                                '<div id="hyphenation-num-zone" style="display: inline-block;"></div>',
+                            '</td>',
+                        '</tr>',
+                        '<tr>',
+                            '<td style="padding-bottom: 5px;">',
+                                '<label class="margin-right-5">' + this.textLimit + '</label>',
+                            '</td>',
+                            '<td style="padding-bottom: 5px;">',
+                                '<div id="hyphenation-num-limit" style="display: inline-block;"></div>',
+                            '</td>',
+                        '</tr>',
+                    '</table>',
                 '</div>'
             ].join('');
 
@@ -117,11 +141,29 @@ define([
                 if (parseInt(newValue)===0) field.setValue(-1);
             }, this));
 
+            this.spnZone = new Common.UI.MetricSpinner({
+                el: $window.find('#hyphenation-num-zone'),
+                step: 0.1,
+                width: 80,
+                defaultUnit : 'cm',
+                value: '1 cm',
+                maxValue: 55.87,
+                minValue: 0
+            });
+            this.spnZone.on('change', _.bind(function(field, newValue, oldValue, eOpts){
+                if (this._changedProps) {
+                    var value = field.getNumberValue();
+                    this._changedProps.setHyphenationZone(parseInt(Common.Utils.Metric.fnRecalcToMM(value) * 72 * 20 / 25.4));
+                }
+            }, this));
+
             this.getChild().find('.dlg-btn').on('click', _.bind(this.onBtnClick, this));
+
+            this.updateMetricUnit();
         },
 
         getFocusedComponents: function() {
-            return [this.chAuto, this.chCaps, this.spnLimit];
+            return [this.chAuto, this.chCaps, this.spnZone, this.spnLimit];
         },
 
         getDefaultFocusableComponent: function () {
@@ -137,6 +179,8 @@ define([
                 this.chCaps.setValue(!!props.isHyphenateCaps(), true);
                 var value = props.getHyphenationLimit() || 0;
                 this.spnLimit.setValue(value!==null && value!==undefined ? (value===0 ? -1 : value) : '', true);
+                value = props.getHyphenationZone();
+                this.spnZone.setValue(value!==null && value!==undefined ? Common.Utils.Metric.fnRecalcFromMM(value * 25.4 / 20 / 72.0) : '', true);
             }
             this._changedProps = props || new AscCommon.AutoHyphenationSettings();
         },
@@ -162,10 +206,16 @@ define([
             return this._changedProps;
         },
 
+        updateMetricUnit: function() {
+            this.spnZone.setDefaultUnit(Common.Utils.Metric.getCurrentMetricName());
+            this.spnZone.setStep(Common.Utils.Metric.getCurrentMetric()==Common.Utils.Metric.c_MetricUnits.pt ? 1 : 0.1);
+        },
+
         textTitle: 'Hyphenation',
         textAuto: 'Automatically hyphenate document',
         textCaps: 'Hyphenate words in CAPS',
         textLimit: 'Limit consecutive hyphens to',
-        textNoLimit: 'No limit'
+        textNoLimit: 'No limit',
+        textZone: 'Hyphenation zone'
     }, DE.Views.HyphenationDialog || {}))
 });
