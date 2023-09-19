329ba2411e5a18d467291eee25d253d226f8b4f1
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2023-09-19 20:05:46 +0300

    [common] fix bug 64202

--- apps/api/documents/api.js
+++ apps/api/documents/api.js
@@ -943,7 +943,7 @@
 
         if (typeof config.documentType === 'string') {
             app = appMap[config.documentType.toLowerCase()];
-            if (!!config.document && typeof config.document.fileType === 'string') {
+            if (config.type == 'desktop' && !!config.document && typeof config.document.fileType === 'string') {
                 var type = /^(?:(pdf|djvu|xps|oxps))$/.exec(config.document.fileType);
                 if (type && typeof type[1] === 'string')
                     app = appMap['pdf'];
