447858660989ea3335ef1ab770ad5f6482a00bad
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2023-12-21 23:23:05 +0300

    Fix for pdf form

--- apps/pdfeditor/main/app/controller/Main.js
+++ apps/pdfeditor/main/app/controller/Main.js
@@ -1196,7 +1196,7 @@ define([
                 this.appOptions.isEdit         = !this.appOptions.isXpsViewer && !this.appOptions.isForm;
                 this.appOptions.canPDFEdit     = false;//(this.permissions.edit !== false) && this.appOptions.canLicense;
                 this.appOptions.isPDFEdit      = false; // this.appOptions.canPDFEdit && this.editorConfig.mode !== 'view'; !! always open in view mode
-                this.appOptions.canPDFAnnotate = (this.appOptions.canSwitchMode || !this.appOptions.isXpsViewer && this.appOptions.isDesktopApp && this.appOptions.isOffline) && this.appOptions.canLicense && (this.permissions.comment!== false);
+                this.appOptions.canPDFAnnotate = (this.appOptions.canSwitchMode || !this.appOptions.isXpsViewer && !this.appOptions.isForm && this.appOptions.isDesktopApp && this.appOptions.isOffline) && this.appOptions.canLicense && (this.permissions.comment!== false);
                 this.appOptions.canPDFAnnotate = this.appOptions.canPDFAnnotate && !((typeof (this.editorConfig.customization) == 'object') && this.editorConfig.customization.comments===false);
                 this.appOptions.isPDFAnnotate  = this.appOptions.canPDFAnnotate && this.appOptions.isDesktopApp && this.appOptions.isOffline; // this.appOptions.canPDFAnnotate && !this.appOptions.isPDFEdit && this.editorConfig.mode !== 'view'; !! online files always open in view mode
                 this.appOptions.canComments    = !this.appOptions.isXpsViewer && !this.appOptions.isForm;
