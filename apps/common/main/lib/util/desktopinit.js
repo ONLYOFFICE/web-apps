018f5732e9bb1b7e4ead1e3daf79aee738322028
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2024-01-16 13:20:21 +0300

    [desktop] for bug 65869

--- apps/common/main/lib/util/desktopinit.js
+++ apps/common/main/lib/util/desktopinit.js
@@ -74,6 +74,6 @@ if ( window.AscDesktopEditor ) {
     }
 
     !window.features && (window.features = {});
-    window.features.size = {width: window.outerWidth, height: outerHeight};
+    window.features.size = {width: window.innerWidth, height: window.innerHeight};
     window.desktop.execCommand('webapps:entry', (window.features && JSON.stringify(window.features)) || '');
 }
