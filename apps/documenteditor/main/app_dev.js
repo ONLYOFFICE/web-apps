b0c65bc00e7b8aa7c38fcb6701e2123bc49ef8c3
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2024-01-25 10:49:59 +0300

    [common] added used components from bootstrap

--- apps/documenteditor/main/app_dev.js
+++ apps/documenteditor/main/app_dev.js
@@ -47,13 +47,14 @@ require.config({
         jquery          : '../vendor/jquery/jquery',
         underscore      : '../vendor/underscore/underscore',
         backbone        : '../vendor/backbone/backbone',
-        bootstrap       : '../vendor/bootstrap/dist/js/bootstrap',
         text            : '../vendor/requirejs-text/text',
         perfectscrollbar: 'common/main/lib/mods/perfect-scrollbar',
         jmousewheel     : '../vendor/perfect-scrollbar/src/jquery.mousewheel',
         xregexp         : '../vendor/xregexp/xregexp-all-min',
         socketio        : '../vendor/socketio/socket.io.min',
         api             : 'api/documents/api',
+        dropdown        : 'common/main/lib/mods/dropdown',
+        tooltip         : 'common/main/lib/mods/tooltip',
         core            : 'common/main/lib/core/application',
         notification    : 'common/main/lib/core/NotificationCenter',
         keymaster       : 'common/main/lib/core/keymaster',
@@ -75,11 +76,6 @@ require.config({
                 'jquery'
             ],
             exports: 'Backbone'
-        },
-        bootstrap: {
-            deps: [
-                'jquery'
-            ]
         },
         perfectscrollbar: {
             deps: [
@@ -113,14 +109,13 @@ require.config({
 
 require([
     'backbone',
-    'bootstrap',
     'core',
     'analytics',
     'gateway',
     'locale',
     'socketio',
 	'underscore'
-], function (Backbone, Bootstrap, Core) {
+], function (Backbone) {
     if (Backbone.History && Backbone.History.started)
         return;
     Backbone.history.start();
@@ -166,6 +161,8 @@ require([
     Common.Locale.apply(
         function() {
             require([
+                'tooltip',
+                'dropdown',
                 'common/main/lib/util/LocalStorage',
                 'common/main/lib/controller/Scaling',
                 'common/main/lib/controller/Themes',
