5f21ec6d99eaaef4faa5086295b417a051c19dbf
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2024-02-14 17:19:38 +0300

    Fix Bug 66456

--- apps/documenteditor/main/app/view/TableOfContentsSettings.js
+++ apps/documenteditor/main/app/view/TableOfContentsSettings.js
@@ -197,7 +197,7 @@ define([
                 }
             }, this));
 
-            this.cmbLeader = new Common.UI.ComboBox({
+            this.cmbLeader = new Common.UI.ComboBoxCustom({
                 el          : $('#tableofcontents-combo-leader'),
                 style       : 'width: 85px;',
                 menuStyle   : 'min-width: 85px;',
@@ -205,11 +205,19 @@ define([
                 takeFocusOnClose: true,
                 cls         : 'input-group-nr',
                 data        : [
-                    { value: Asc.c_oAscTabLeader.None,      displayValue: this.textNone },
-                    { value: Asc.c_oAscTabLeader.Dot,       displayValue: '....................' },
-                    { value: Asc.c_oAscTabLeader.Hyphen,    displayValue: '-----------------' },
-                    { value: Asc.c_oAscTabLeader.Underscore,displayValue: '__________' }
-                ]
+                    { value: Asc.c_oAscTabLeader.None,      cls: '', displayValue: this.textNone },
+                    { value: Asc.c_oAscTabLeader.Dot,       cls: 'font-sans-serif', displayValue: '....................' },
+                    { value: Asc.c_oAscTabLeader.Hyphen,    cls: 'font-sans-serif', displayValue: '-----------------' },
+                    { value: Asc.c_oAscTabLeader.Underscore,cls: 'font-sans-serif', displayValue: '__________' }
+                ],
+                itemsTemplate: _.template([
+                    '<% _.each(items, function(item) { %>',
+                    '<li id="<%= item.id %>" data-value="<%- item.value %>" class="<%= item.cls %>"><a tabindex="-1" type="menuitem"><%= scope.getDisplayValue(item) %></a></li>',
+                    '<% }); %>',
+                ].join('')),
+                updateFormControl: function(record) {
+                    this._input && this._input.toggleClass('font-sans-serif', record.get('value')!==Asc.c_oAscTabLeader.None);
+                }
             });
             this.cmbLeader.setValue(Asc.c_oAscTabLeader.Dot);
             this.cmbLeader.on('selected', _.bind(function(combo, record) {
