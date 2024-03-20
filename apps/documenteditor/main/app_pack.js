c860ceec77da9801359ce2fc73756005a18c2283
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2024-03-20 14:45:09 +0300

    [DE] added config for 'postload' script

---
+++ apps/documenteditor/main/app_pack.js
@@ -0,0 +1,8 @@
+require([
+    "common/main/lib/controller/ScreenReaderFocus",
+    "documenteditor/main/app/view/ParagraphSettingsAdvanced",
+    "documenteditor/main/app/view/ImageSettingsAdvanced.js",
+    "documenteditor/main/app/view/TableSettingsAdvanced.js",
+    "documenteditor/main/app/view/DropcapSettingsAdvanced.js",
+], function () {
+});
