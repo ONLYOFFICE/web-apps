ea1a0d8a228451f57b97c1e1bcb75a671451ae33
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2024-05-20 16:53:56 +0300

    [main] fix bug 68074

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -473,6 +473,7 @@ module.exports = function(grunt) {
                         params: {
                             overrides: {
                                 cleanupIds: false,
+                                removeHiddenElems: false,   // plugin ver 3.2.0 deletes <symbol> as non rendering element
                             }
                         },
                     },
