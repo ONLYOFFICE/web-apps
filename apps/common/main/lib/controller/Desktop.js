027e80f36016d4e9f2a6db93c9d4ff84934f5dc7
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2024-06-17 10:58:58 +0300

    [desktop] fix bug 68666

--- apps/common/main/lib/controller/Desktop.js
+++ apps/common/main/lib/controller/Desktop.js
@@ -616,7 +616,7 @@ define([
                                     menu.hide();
                                 } else
                                 if ( action == 'create:fromtemplate' ) {
-                                    native.execCommand('create:new', 'template:' + (!!window.SSE ? 'cell' : !!window.PE ? 'slide' : 'word'));
+                                    native.execCommand('create:new', 'template:' + (!!window.SSE ? 'cell' : !!window.PE ? 'slide' : !!window.PDFE ? 'form' : 'word'));
                                     menu.hide();
                                 }
                             },
