f9106dc54717e1ec157f6eb4bb8c55264c6dcc02
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2024-08-10 21:04:38 +0300

    Delay loading custom menu items

--- apps/pdfeditor/main/app/view/DocumentHolder.js
+++ apps/pdfeditor/main/app/view/DocumentHolder.js
@@ -1617,137 +1617,6 @@ define([
             return container;
         },
 
-        updateCustomItems: function(menu, data) {
-            if (!menu || !data || data.length<1) return;
-
-            var me = this,
-                lang = me.mode && me.mode.lang ? me.mode.lang.split(/[\-_]/)[0] : 'en';
-
-            me._preventCustomClick && clearTimeout(me._preventCustomClick);
-            me._hasCustomItems && (me._preventCustomClick = setTimeout(function () {
-                me._preventCustomClick = null;
-            },500)); // set delay only on update existing items
-            me._hasCustomItems = true;
-
-            var findCustomItem = function(guid, id) {
-                if (menu && menu.items.length>0) {
-                    for (var i = menu.items.length-1; i >=0 ; i--) {
-                        if (menu.items[i].options.isCustomItem && (id===undefined && menu.items[i].options.guid === guid || menu.items[i].options.guid === guid && menu.items[i].value === id)) {
-                            return menu.items[i];
-                        }
-                    }
-                }
-            }
-
-            var getMenu = function(items, guid, toMenu) {
-                if (toMenu)
-                    toMenu.removeAll();
-                else {
-                    toMenu = new Common.UI.Menu({
-                        cls: 'shifted-right',
-                        menuAlign: 'tl-tr',
-                        items: []
-                    });
-                    toMenu.on('item:click', function(menu, item, e) {
-                        !me._preventCustomClick && me.api && me.api.onPluginContextMenuItemClick && me.api.onPluginContextMenuItemClick(item.options.guid, item.value);
-                    });
-                    toMenu.on('menu:click', function(menu, e) {
-                        me._preventCustomClick && e.stopPropagation();
-                    });
-                }
-                items.forEach(function(item) {
-                    item.separator && toMenu.addItem({
-                        caption: '--',
-                        isCustomItem: true,
-                        guid: guid
-                    });
-                    item.text && toMenu.addItem({
-                        caption: ((typeof item.text == 'object') ? item.text[lang] || item.text['en'] : item.text) || '',
-                        isCustomItem: true,
-                        value: item.id,
-                        guid: guid,
-                        menu: item.items ? getMenu(item.items, guid) : false,
-                        iconImg: me.parseIcons(item.icons),
-                        disabled: !!item.disabled
-                    });
-                });
-                return toMenu;
-            }
-
-            var focused;
-            data.forEach(function(plugin) {
-                var isnew = !findCustomItem(plugin.guid);
-                if (plugin && plugin.items && plugin.items.length>0) {
-                    plugin.items.forEach(function(item) {
-                        if (item.separator && isnew) {// add separator only to new plugins menu
-                            menu.addItem({
-                                caption: '--',
-                                isCustomItem: true,
-                                guid: plugin.guid
-                            });
-                        }
-
-                        if (!item.text) return;
-                        var mnu = findCustomItem(plugin.guid, item.id),
-                            caption = ((typeof item.text == 'object') ? item.text[lang] || item.text['en'] : item.text) || '';
-                        if (mnu) {
-                            mnu.setCaption(caption);
-                            mnu.setDisabled(!!item.disabled);
-                            if (item.items) {
-                                if (mnu.menu) {
-                                    if (mnu.menu.isVisible() && mnu.menu.cmpEl.find(' > li:not(.divider):not(.disabled):visible').find('> a').filter(':focus').length>0) {
-                                        mnu.menu.isOver = true;
-                                        focused = mnu.cmpEl;
-                                    }
-                                    getMenu(item.items, plugin.guid, mnu.menu);
-                                } else
-                                    mnu.setMenu(getMenu(item.items, plugin.guid));
-                            }
-                        } else {
-                            var mnu = new Common.UI.MenuItem({
-                                caption     : caption,
-                                isCustomItem: true,
-                                value: item.id,
-                                guid: plugin.guid,
-                                menu: item.items && item.items.length>=0 ? getMenu(item.items, plugin.guid) : false,
-                                iconImg: me.parseIcons(item.icons),
-                                disabled: !!item.disabled
-                            }).on('click', function(item, e) {
-                                !me._preventCustomClick && me.api && me.api.onPluginContextMenuItemClick && me.api.onPluginContextMenuItemClick(item.options.guid, item.value);
-                            });
-                            menu.addItem(mnu);
-                        }
-                    });
-                }
-            });
-
-            if (focused) {
-                var $subitems = $('> [role=menu]', focused).find('> li:not(.divider):not(.disabled):visible > a');
-                ($subitems.length>0) && $subitems.eq(0).focus();
-            }
-            menu.alignPosition();
-        },
-
-        clearCustomItems: function(menu) {
-            if (menu && menu.items.length>0) {
-                for (var i = 0; i < menu.items.length; i++) {
-                    if (menu.items[i].options.isCustomItem) {
-                        menu.removeItem(menu.items[i]);
-                        i--;
-                    }
-                }
-            }
-            this._hasCustomItems = false;
-        },
-
-        parseIcons: function(icons) {
-            var plugins = PDFE.getController('Common.Controllers.Plugins').getView('Common.Views.Plugins');
-            if (icons && icons.length && plugins && plugins.parseIcons) {
-                icons = plugins.parseIcons(icons);
-                return icons ? icons['normal'] : undefined;
-            }
-        },
-
         focus: function() {
             var me = this;
             _.defer(function(){  me.cmpEl.focus(); }, 50);
@@ -1756,6 +1625,20 @@ define([
         SetDisabled: function(state, canProtect, fillFormMode) {
             this._isDisabled = state;
         },
+
+        addEquationMenu: function() {},
+
+        clearEquationMenu: function() {},
+
+        equationCallback: function() {},
+
+        initEquationMenu: function() {},
+
+        updateCustomItems: function() {},
+
+        clearCustomItems: function() {},
+
+        parseIcons: function() {},
 
         textCopy: 'Copy',
         addCommentText: 'Add Comment',
--- apps/spreadsheeteditor/main/app/view/DocumentHolder.js
+++ apps/spreadsheeteditor/main/app/view/DocumentHolder.js
@@ -1580,136 +1580,19 @@ define([
             });
         },
 
-        updateCustomItems: function(menu, data) {
-            if (!menu || !data || data.length<1) return;
+        addEquationMenu: function() {},
 
-            var me = this,
-                lang = me.mode && me.mode.lang ? me.mode.lang.split(/[\-_]/)[0] : 'en';
+        clearEquationMenu: function() {},
 
-            me._preventCustomClick && clearTimeout(me._preventCustomClick);
-            me._hasCustomItems && (me._preventCustomClick = setTimeout(function () {
-                me._preventCustomClick = null;
-            },500)); // set delay only on update existing items
-            me._hasCustomItems = true;
+        equationCallback: function() {},
 
-            var findCustomItem = function(guid, id) {
-                if (menu && menu.items.length>0) {
-                    for (var i = menu.items.length-1; i >=0 ; i--) {
-                        if (menu.items[i].options.isCustomItem && (id===undefined && menu.items[i].options.guid === guid || menu.items[i].options.guid === guid && menu.items[i].value === id)) {
-                            return menu.items[i];
-                        }
-                    }
-                }
-            }
+        initEquationMenu: function() {},
 
-            var getMenu = function(items, guid, toMenu) {
-                if (toMenu)
-                    toMenu.removeAll();
-                else {
-                    toMenu = new Common.UI.Menu({
-                        cls: 'shifted-right',
-                        menuAlign: 'tl-tr',
-                        items: []
-                    });
-                    toMenu.on('item:click', function(menu, item, e) {
-                        !me._preventCustomClick && me.api && me.api.onPluginContextMenuItemClick && me.api.onPluginContextMenuItemClick(item.options.guid, item.value);
-                    });
-                    toMenu.on('menu:click', function(menu, e) {
-                        me._preventCustomClick && e.stopPropagation();
-                    });
-                }
-                items.forEach(function(item) {
-                    item.separator && toMenu.addItem({
-                        caption: '--',
-                        isCustomItem: true,
-                        guid: guid
-                    });
-                    item.text && toMenu.addItem({
-                        caption: ((typeof item.text == 'object') ? item.text[lang] || item.text['en'] : item.text) || '',
-                        isCustomItem: true,
-                        value: item.id,
-                        guid: guid,
-                        menu: item.items ? getMenu(item.items, guid) : false,
-                        iconImg: me.parseIcons(item.icons),
-                        disabled: !!item.disabled
-                    });
-                });
-                return toMenu;
-            }
+        updateCustomItems: function() {},
 
-            var focused;
-            data.forEach(function(plugin) {
-                var isnew = !findCustomItem(plugin.guid);
-                if (plugin && plugin.items && plugin.items.length>0) {
-                    plugin.items.forEach(function(item) {
-                        if (item.separator && isnew) {// add separator only to new plugins menu
-                            menu.addItem({
-                                caption: '--',
-                                isCustomItem: true,
-                                guid: plugin.guid
-                            });
-                        }
+        clearCustomItems: function() {},
 
-                        if (!item.text) return;
-                        var mnu = findCustomItem(plugin.guid, item.id),
-                            caption = ((typeof item.text == 'object') ? item.text[lang] || item.text['en'] : item.text) || '';
-                        if (mnu) {
-                            mnu.setCaption(caption);
-                            mnu.setDisabled(!!item.disabled);
-                            if (item.items) {
-                                if (mnu.menu) {
-                                    if (mnu.menu.isVisible() && mnu.menu.cmpEl.find(' > li:not(.divider):not(.disabled):visible').find('> a').filter(':focus').length>0) {
-                                        mnu.menu.isOver = true;
-                                        focused = mnu.cmpEl;
-                                    }
-                                    getMenu(item.items, plugin.guid, mnu.menu);
-                                } else
-                                    mnu.setMenu(getMenu(item.items, plugin.guid));
-                            }
-                        } else {
-                            var mnu = new Common.UI.MenuItem({
-                                caption     : caption,
-                                isCustomItem: true,
-                                value: item.id,
-                                guid: plugin.guid,
-                                menu: item.items && item.items.length>=0 ? getMenu(item.items, plugin.guid) : false,
-                                iconImg: me.parseIcons(item.icons),
-                                disabled: !!item.disabled
-                            }).on('click', function(item, e) {
-                                !me._preventCustomClick && me.api && me.api.onPluginContextMenuItemClick && me.api.onPluginContextMenuItemClick(item.options.guid, item.value);
-                            });
-                            menu.addItem(mnu);
-                        }
-                    });
-                }
-            });
-
-            if (focused) {
-                var $subitems = $('> [role=menu]', focused).find('> li:not(.divider):not(.disabled):visible > a');
-                ($subitems.length>0) && $subitems.eq(0).focus();
-            }
-            menu.alignPosition();
-        },
-
-        clearCustomItems: function(menu) {
-            if (menu && menu.items.length>0) {
-                for (var i = 0; i < menu.items.length; i++) {
-                    if (menu.items[i].options.isCustomItem) {
-                        menu.removeItem(menu.items[i]);
-                        i--;
-                    }
-                }
-            }
-            this._hasCustomItems = false;
-        },
-
-        parseIcons: function(icons) {
-            var plugins = SSE.getController('Common.Controllers.Plugins').getView('Common.Views.Plugins');
-            if (icons && icons.length && plugins && plugins.parseIcons) {
-                icons = plugins.parseIcons(icons);
-                return icons ? icons['normal'] : undefined;
-            }
-        },
+        parseIcons: function() {},
 
         txtSort:                'Sort',
         txtAscending:           'Ascending',
