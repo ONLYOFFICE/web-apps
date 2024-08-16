fc63ee5b487ac3859d9bab1a2805ffec1bfba1f7
SergeyEzhin <sergey.ezhin@onlyoffice.com>
2024-08-16 15:03:27 +0300

    [common] Changing webpack and babel config

--- vendor/framework7-react/babel.config.js
+++ vendor/framework7-react/babel.config.js
@@ -1,16 +1,24 @@
 const config = {
   presets: [
-    '@babel/preset-react',
-    ['@babel/preset-env', {
-      modules: false,
-    }],
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: false,
+        targets: "> 0.25%, not dead",
+      }
+    ],
+    "@babel/preset-react"
   ],
   plugins: [
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-syntax-dynamic-import',
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-dynamic-import",
     ['@babel/plugin-proposal-decorators', {'legacy': true }],
-    ['@babel/plugin-transform-class-properties', { 'loose': false }]
-  ],
+    ['@babel/plugin-transform-class-properties', { 'loose': false }],
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-transform-react-inline-elements",
+    "@babel/plugin-transform-react-constant-elements",
+  ]
 };
 
 export default config;
