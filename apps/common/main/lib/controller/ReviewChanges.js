477f1fa93c66ec02cdb4940111b1d65da7e87e81
Konstantin Kireyev <konstantin.kireyev@onlyoffice.com>
2024-08-26 02:10:36 +0500

    feat/de: text from file

--- apps/common/main/lib/controller/ReviewChanges.js
+++ apps/common/main/lib/controller/ReviewChanges.js
@@ -159,11 +159,9 @@ define([
                 this.api.asc_registerCallback('asc_onCoAuthoringDisconnect',_.bind(this.onCoAuthoringDisconnect, this));
 
                 Common.Gateway.on('setrevisedfile', _.bind(this.setRevisedFile, this));
-                if (this.appConfig.canFeatureComparison) {
-                    Common.NotificationCenter.on('storage:document-load', _.bind(this.openDocumentFromStorage, this));
-                    Common.NotificationCenter.on('storage:document-insert', _.bind(this.insertDocumentFromStorage, this));
-                    Common.Gateway.on('setrequesteddocument', _.bind(this.setRequestedDocument, this));
-                }
+                Common.NotificationCenter.on('storage:document-load', _.bind(this.openDocumentFromStorage, this));
+                Common.NotificationCenter.on('storage:document-insert', _.bind(this.insertDocumentFromStorage, this));
+                Common.Gateway.on('setrequesteddocument', _.bind(this.setRequestedDocument, this));
             }
         },
 
