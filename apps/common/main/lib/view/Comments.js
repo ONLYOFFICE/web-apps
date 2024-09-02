e0edf94b988f2178b6dbdf29c86afcd363c27915
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2024-09-02 17:55:32 +0300

    Fix review/comment popover

--- apps/common/main/lib/view/Comments.js
+++ apps/common/main/lib/view/Comments.js
@@ -163,12 +163,12 @@ define([
             autoScrollToEditButtons: function () {
                 var button = $('#id-comments-change'),  // TODO: add to cache
                     btnBounds = null,
-                    contentBounds = this.el.getBoundingClientRect(),
+                    contentBounds = Common.Utils.getBoundingClientRect(this.el),
                     moveY = 0,
                     padding = 7;
 
                 if (button.length) {
-                    btnBounds = button.get(0).getBoundingClientRect();
+                    btnBounds = Common.Utils.getBoundingClientRect(button.get(0));
                     if (btnBounds && contentBounds) {
                         moveY = contentBounds.bottom - (btnBounds.bottom + padding);
                         if (moveY < 0) {
