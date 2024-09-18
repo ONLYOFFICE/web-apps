d7501fcb929a49dcee1a3b3ac0f5d1cacc4eaf99
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2024-09-18 16:56:56 +0300

    [SSE] Fix bug 69521

--- apps/spreadsheeteditor/main/app/view/FileMenuPanels.js
+++ apps/spreadsheeteditor/main/app/view/FileMenuPanels.js
@@ -1286,8 +1286,8 @@ define([], function () {
                     info.asc_setSymbol(landId);
                     var arr = this.api.asc_getFormatCells(info); // all formats
                     text = this.api.asc_getLocaleExample(arr[4], 1000.01, landId);
-                    text = text + ' ' + this.api.asc_getLocaleExample(arr[5], Asc.cDate().getExcelDateWithTime(), landId);
-                    text = text + ' ' + this.api.asc_getLocaleExample(arr[7], Asc.cDate().getExcelDateWithTime(), landId);
+                    text = text + ' ' + this.api.asc_getLocaleExample(arr[5], Asc.cDate().getExcelDateWithTime(true), landId);
+                    text = text + ' ' + this.api.asc_getLocaleExample(arr[7], Asc.cDate().getExcelDateWithTime(true), landId);
                 }
                 $('#fms-lbl-reg-settings').text(_.isEmpty(text) ? '' : this.strRegSettingsEx + text);
             }
