6ca40ad32551fa69810f81f86b5a9806454cbb69
Julia Radzhabova <Julia.Radzhabova@onlyoffice.com>
2024-10-02 19:35:56 +0300

    Change text for update version event

--- apps/documenteditor/main/app/controller/Toolbar.js
+++ apps/documenteditor/main/app/controller/Toolbar.js
@@ -257,9 +257,9 @@ define([
             this.toolbar.applyLayout(mode);
             !this.mode.isPDFForm && Common.UI.TooltipManager.addTips({
                 'pageColor' : {name: 'de-help-tip-page-color', placement: 'bottom-left', text: this.helpPageColor, header: this.helpPageColorHeader, target: '#slot-btn-pagecolor', automove: true},
-                'refreshFile' : {text: _main.textUpdateVersion, target: '#toolbar', maxwidth: 'none', showButton: false, automove: true, noHighlight: true, multiple: true},
-                'disconnect' : {text: _main.textConnectionLost, target: '#toolbar', maxwidth: 'none', showButton: false, automove: true, noHighlight: true, multiple: true},
-                'updateVersion' : {text: _main.errorUpdateVersionOnDisconnect, target: '#toolbar', maxwidth: 600, showButton: false, automove: true, noHighlight: true, multiple: true}
+                'refreshFile' : {text: _main.textUpdateVersion, header: _main.textUpdating, target: '#toolbar', maxwidth: 'none', showButton: false, automove: true, noHighlight: true, multiple: true},
+                'disconnect' : {text: _main.textConnectionLost, header: _main.textDisconnect, target: '#toolbar', maxwidth: 'none', showButton: false, automove: true, noHighlight: true, multiple: true},
+                'updateVersion' : {text: _main.errorUpdateVersionOnDisconnect, header: _main.titleUpdateVersion, target: '#toolbar', maxwidth: 600, showButton: false, automove: true, noHighlight: true, multiple: true}
             });
         },
 
