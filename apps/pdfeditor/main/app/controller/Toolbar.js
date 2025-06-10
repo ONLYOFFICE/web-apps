6e50641aa54d6ff8330d2013e40119028b29e549
Julia.Radzhabova <Julia.Radzhabova@onlyoffice.com>
2025-06-10 19:03:11 +0300

    [PDF] Add new features tips

--- apps/pdfeditor/main/app/controller/Toolbar.js
+++ apps/pdfeditor/main/app/controller/Toolbar.js
@@ -182,7 +182,10 @@ define([
                 isNew = themeid==='theme-system' || themeid==='theme-white' || themeid==='theme-night';
 
             Common.UI.FeaturesManager.isFeatureEnabled('featuresTips', true) && Common.UI.TooltipManager.addTips({
-                'modernTheme' : {name: 'help-tip-modern-theme', placement: 'bottom', text: isNew ? this.helpOldTheme : this.helpModernTheme, header: this.helpModernThemeHeader, target: '#slot-btn-interface-theme', automove: true, maxwidth: 270, closable: false, isNewFeature: true},
+                'modernTheme' : {name: 'help-tip-modern-theme', placement: 'bottom-right', text: isNew ? this.helpOldTheme : this.helpModernTheme, header: this.helpModernThemeHeader, target: '#slot-btn-interface-theme', automove: true, maxwidth: 270, closable: false, isNewFeature: true},
+                'formEditor' : {name: 'help-tip-form-editor', placement: 'bottom-right', offset: {x: 10, y: 0}, text: this.helpFormEditor, header: this.helpFormEditorHeader, target: 'li.ribtab #forms', automove: true, maxwidth: 270, closable: false, isNewFeature: true},
+                'copyPages' : {name: 'help-tip-copy-pages', placement: 'right-bottom', offset: {x: -30, y: 110}, text: Common.Utils.String.format(this.helpCopyPages, Common.Utils.String.platformKey('Ctrl+C', '{0}'), Common.Utils.String.platformKey('Ctrl+V', '{0}')),
+                               header: this.helpCopyPagesHeader, target: '#thumbnails-btn-close', closable: false, isNewFeature: true},
                 // 'addStamp' : {name: 'help-tip-add-stamp', placement: 'bottom-left', text: this.helpAddStamp, header: this.helpAddStampHeader, target: '#slot-btn-stamp', closable: false},
                 // 'selectPages' : {name: 'help-tip-select-pages', placement: 'right-bottom', offset: {x: -30, y: 60}, text: this.helpSelectPages, header: this.helpSelectPagesHeader, target: '#thumbnails-btn-close', closable: false},
                 // 'fastUndo' : {name: 'pdfe-help-tip-fast-undo', placement: 'bottom-right', text: this.helpFastUndo, header: this.helpFastUndoHeader, target: mode.compactHeader ? '#slot-btn-undo' : '#slot-btn-dt-undo', extCls: 'inc-index', closable: false}
@@ -516,10 +519,10 @@ define([
                     this.toolbar.lockToolbar(Common.enumLock.undoLock, !can, {array: [this.toolbar.btnUndo]});
                     this._state.can_undo = can;
 
-                    if (can) {
-                        var _main = this.getApplication().getController('Main');
-                        _main._state.fastCoauth && _main._state.usersCount>1 && Common.UI.TooltipManager.showTip('fastUndo');
-                    }
+                    // if (can) {
+                    //     var _main = this.getApplication().getController('Main');
+                    //     _main._state.fastCoauth && _main._state.usersCount>1 && Common.UI.TooltipManager.showTip('fastUndo');
+                    // }
                 }
             } else {
                 if (this._state.can_redo !== can) {
@@ -878,7 +881,7 @@ define([
         },
 
         onUndo: function(btn, e) {
-            Common.UI.TooltipManager.closeTip('fastUndo');
+            // Common.UI.TooltipManager.closeTip('fastUndo');
 
             if (this.api)
                 this.api.Undo();
@@ -1219,7 +1222,7 @@ define([
         },
 
         onBtnStampClick: function(btn, e) {
-            Common.UI.TooltipManager.closeTip('addStamp');
+            // Common.UI.TooltipManager.closeTip('addStamp');
             this.onInsertStamp(btn.options.stampType, btn, e);
         },
 
@@ -1241,7 +1244,7 @@ define([
         },
 
         onStampShowAfter: function(menu) {
-            Common.UI.TooltipManager.closeTip('addStamp');
+            // Common.UI.TooltipManager.closeTip('addStamp');
             if (menu.getItemsLength(true)<1 && this.api) {
                 var arr = this.api.asc_getPropertyEditorStamps(),
                     template = _.template([
@@ -1521,6 +1524,8 @@ define([
             toolbar.setVisible('forms', this.mode.isPDFEdit && this.mode.canFeatureForms);
             $host.find('.annotate').toggleClass('hidden', this.mode.isPDFEdit);
             $host.find('.pdfedit').toggleClass('hidden', !this.mode.isPDFEdit);
+
+            this.mode.isPDFEdit ? Common.UI.TooltipManager.showTip('formEditor') : Common.UI.TooltipManager.closeTip('formEditor');
         },
         
         onAppReady: function (config) {
@@ -1584,11 +1589,16 @@ define([
                 this.requiredTooltip = undefined;
             }
             // (tab === 'comment') ? Common.UI.TooltipManager.showTip('addStamp') : Common.UI.TooltipManager.closeTip('addStamp');
-            // if (tab === 'file') {
-            //     Common.UI.TooltipManager.closeTip('selectPages');
+            if (tab === 'file') {
+                Common.UI.TooltipManager.closeTip('copyPages');
             //     Common.UI.TooltipManager.closeTip('fastUndo');
-            // }
-            (tab === 'view') ? Common.UI.TooltipManager.showTip('modernTheme') : Common.UI.TooltipManager.closeTip('modernTheme');
+            }
+            if (tab === 'view') {
+                Common.UI.TooltipManager.getNeedShow('modernTheme') && Common.UI.TooltipManager.closeTip('formEditor');
+                Common.UI.TooltipManager.showTip('modernTheme')
+            } else
+                Common.UI.TooltipManager.closeTip('modernTheme');
+            (tab === 'forms') && Common.UI.TooltipManager.closeTip('formEditor');
         },
 
         onTabCollapse: function(tab) {
