f3a2855fd51e399a45cd1bc77b160bdf63027a8b
konstantin.kireyev <konstantin.kireyev@onlyoffice.com>
2025-06-25 14:43:31 +0500

    fix: mobile build

--- vendor/framework7-react/postcss.config.js
+++ vendor/framework7-react/postcss.config.js
@@ -1,18 +1,18 @@
-import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss'
+import postcssPresetEnv from 'postcss-preset-env';
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
 
 const config = (ctx) => ({
-  plugins: {
-    'postcss-preset-env': {},
-    ...(ctx.env === 'production' ?
-      purgeCSSPlugin({
-        content: [
-          './src/**/*.html',
-          './src/**/*.jsx',
-          './src/**/*.js',
-        ],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    }) : {})
-  },
+	plugins: [
+		postcssPresetEnv(),
+		ctx.env === 'production' ? purgeCSSPlugin({
+			content: [
+				'./src/**/*.html',
+				'./src/**/*.jsx',
+				'./src/**/*.js',
+			],
+			defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+		}) : null,
+	].filter(Boolean),
 });
 
 export default config;
