a97b2c3b557c4fcebf197835f1534cb5879de8cc
Julia.Radzhabova <Julia.Radzhabova@onlyoffice.com>
2025-06-30 17:48:25 +0300

    Check the onOutdatedVersion event

--- apps/visioeditor/main/app/controller/Main.js
+++ apps/visioeditor/main/app/controller/Main.js
@@ -1705,7 +1705,7 @@ define([
             },
 
             onUpdateVersion: function(callback) {
-                console.log("Obsolete: The 'onOutdatedVersion' event is deprecated. Please use 'onRequestRefreshFile' event and 'refreshFile' method instead.");
+                this.editorConfig && this.editorConfig.canUpdateVersion && console.log("Obsolete: The 'onOutdatedVersion' event is deprecated. Please use 'onRequestRefreshFile' event and 'refreshFile' method instead.");
 
                 var me = this;
                 me.needToUpdateVersion = true;
