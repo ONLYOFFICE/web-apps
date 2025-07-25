ae4cdc38aa1d138074221ed78069e24ef89a04f6
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2025-07-25 16:00:01 +0300

    [PE][mobile] for bug 75827

--- apps/presentationeditor/mobile/src/controller/Preview.jsx
+++ apps/presentationeditor/mobile/src/controller/Preview.jsx
@@ -19,9 +19,9 @@ const PreviewController = props => {
         ContextMenu.closeContextMenu();
 
         _view = $$('#pe-preview');
-        _view.on('touchstart', onTouchStart);
-        _view.on('touchmove', onTouchMove);
-        _view.on('touchend', onTouchEnd);
+        // _view.on('touchstart', onTouchStart);
+        // _view.on('touchmove', onTouchMove);
+        // _view.on('touchend', onTouchEnd);
 
         show();
         onDocumentReady();
@@ -31,9 +31,9 @@ const PreviewController = props => {
 
             api.asc_unregisterCallback('asc_onEndDemonstration', onEndDemonstration);
         
-            _view.off('touchstart', onTouchStart);
-            _view.off('touchmove', onTouchMove);
-            _view.off('touchend', onTouchEnd);
+            // _view.off('touchstart', onTouchStart);
+            // _view.off('touchmove', onTouchMove);
+            // _view.off('touchend', onTouchEnd);
         };
     }, []);
 
