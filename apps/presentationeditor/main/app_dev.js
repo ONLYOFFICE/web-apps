5c9005319a3c2c3cb9181afcba8b237a054c9024
Alexey Koshelev <Basher0303@yandex.ru>
2025-10-09 14:43:04 +0300

    [common] Hide shortcuts settings

--- apps/presentationeditor/main/app_dev.js
+++ apps/presentationeditor/main/app_dev.js
@@ -230,8 +230,8 @@ require([
                 'common/main/lib/view/DocumentPropertyDialog',
                 'common/main/lib/view/MacrosDialog',
                 'common/main/lib/view/MacrosAiDialog',
-                'common/main/lib/view/ShortcutsDialog',
-                'common/main/lib/view/ShortcutsEditDialog',
+                // 'common/main/lib/view/ShortcutsDialog',
+                // 'common/main/lib/view/ShortcutsEditDialog',
                 'common/main/lib/component/MonacoEditor',
                 'common/main/lib/component/TextareaField',
 
