4d235ad893d20615a4264fe87785a4f4e1cd5ec7
Julia.Radzhabova <Julia.Radzhabova@onlyoffice.com>
2025-11-21 15:38:35 +0300

    [DE] For bug 78627: show warning if copying is disabled

--- apps/documenteditor/main/app/controller/DocumentHolderExt.js
+++ apps/documenteditor/main/app/controller/DocumentHolderExt.js
@@ -2140,7 +2140,7 @@ define([], function () {
             if (me.api) {
                 var res =  (item.value == 'cut') ? me.api.Cut() : ((item.value == 'copy') ? me.api.Copy() : me.api.Paste());
                 if (!res) {
-                    if (!Common.localStorage.getBool("de-hide-copywarning")) {
+                    if (!Common.localStorage.getBool("de-hide-copywarning") && (item.value === 'paste' || me.mode.canCopy)) {
                         (new Common.Views.CopyWarningDialog({
                             handler: function(dontshow) {
                                 if (dontshow) Common.localStorage.setItem("de-hide-copywarning", 1);
