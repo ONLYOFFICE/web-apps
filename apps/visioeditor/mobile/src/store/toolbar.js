5271ea241e0b8ae9214f7327f1bf9ca46ab79f7a
Dmitry-Ilyushechkin <dmitry.ilyushechkin@onlyoffice.com>
2025-12-18 16:59:45 +0300

    [Visio] An “Open Location” button has been added to the menu.

--- apps/visioeditor/mobile/src/store/toolbar.js
+++ apps/visioeditor/mobile/src/store/toolbar.js
@@ -29,4 +29,10 @@ export class storeToolbarSettings {
     setCountPages(count) {
         this.countPages = count;
     }
+
+    isShowBack = false;
+
+    setShowBack = (value) => {
+        this.isShowBack = !!value;
+    };
 }
