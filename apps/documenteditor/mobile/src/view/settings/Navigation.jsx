71a61d37581ac4e168303f6cf067800c4eb7c4a4
Polina <polina.hudyakova@onlyoffice.com>
2026-01-30 09:49:32 +0300

    swiper-by-brakepoint

--- apps/documenteditor/mobile/src/view/settings/Navigation.jsx
+++ apps/documenteditor/mobile/src/view/settings/Navigation.jsx
@@ -60,67 +60,26 @@ const NavigationSheet = inject('storeNavigation')(observer(props => {
     } else if(bookmarks.length) {
         arrHeaders = props.updateViewerNavigation(bookmarks);
     }
-
-    const [stateHeight, setHeight] = useState('45%');
-    const [stateOpacity, setOpacity] = useState(1);
-
-    const [stateStartY, setStartY] = useState();
-    const [isNeedClose, setNeedClose] = useState(false);
-
-    const handleTouchStart = (event) => {
-        const touchObj = event.changedTouches[0];
-        setStartY(parseInt(touchObj.clientY));
-    };
-
-    const handleTouchMove = (event) => {
-        const touchObj = event.changedTouches[0];
-        const dist = parseInt(touchObj.clientY) - stateStartY;
-
-        if (dist < 0) { 
-            setHeight('90%');
-            setOpacity(1);
-            setNeedClose(false);
-        } else if (dist < 80) {
-            setHeight('45%');
-            setOpacity(1);
-            setNeedClose(false);
-        } else {
-            setNeedClose(true);
-            setOpacity(0.6);
-        }
-    };
-
-    const handleTouchEnd = (event) => {
-        const touchObj = event.changedTouches[0];
-        const swipeEnd = parseInt(touchObj.clientY);
-        const dist = swipeEnd - stateStartY;
-
-        if (isNeedClose) {
-            f7.sheet.close('#view-navigation-sheet');
-        } else if (stateHeight === '90%' && dist > 20) {
-            setHeight('45%');
-        }
-    };
-
+    
     useEffect(() => {
         f7.sheet.open('#view-navigation-sheet', true);
     }, []);
 
     return (
-        <Sheet id="view-navigation-sheet" className="navigation-sheet" backdrop={true} closeByBackdropClick={true} onSheetClosed={() => mainContext.closeOptions('navigation')} style={{height: `${stateHeight}`, opacity: `${stateOpacity}`}}>
-            <div id='swipe-handler' className='swipe-container' onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
-                <Icon icon='icon icon-swipe'/>
+        <Sheet id="view-navigation-sheet" className="navigation-sheet" onSheetClosed={() => mainContext.closeOptions('navigation')} backdrop swipeToClose breakpoints={[0.50]} style={{height: '90%'}}>
+            <div id='swipe-handler' className="swipe-container">
+               <Icon icon='icon icon-swipe'/>
             </div>
             <div className="navigation-sheet__title">
                 <p>{t('Settings.textNavigation')}</p>
             </div>
             {!arrHeaders || !arrHeaders.length 
                 ?
-                    <div className="empty-screens">
+                    <div className="empty-screens sheet-modal-swipe-step">
                         <p className="empty-screens__text">{t('Settings.textEmptyScreens')}</p>
                     </div>
                 :
-                    <List className="navigation-list">
+                    <List className="navigation-list sheet-modal-swipe-step">
                         {arrHeaders.map((header, index) => {
                             return (
                                 <ListItem radio key={index} title={header.isEmptyItem ? t('Settings.textBeginningDocument') : header.name} checked={header.index === currentPosition} style={{paddingLeft: header.level * 16}} onClick={() => {
