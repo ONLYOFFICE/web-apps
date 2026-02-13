801782e6e10c894338cbdbfad88351adf3ace1e3
Alexey Koshelev <Basher0303@yandex.ru>
2026-02-13 12:21:51 +0300

    [PDFE] Ask password to print document

--- apps/documenteditor/embed/js/ApplicationController.js
+++ apps/documenteditor/embed/js/ApplicationController.js
@@ -532,8 +532,24 @@ DE.ApplicationController = new(function(){
 
         DE.ApplicationView.tools.get('#idt-print')
             .on('click', function(){
-                api.asc_Print(new Asc.asc_CDownloadOptions(null, $.browser.chrome || $.browser.safari || $.browser.opera || $.browser.mozilla && $.browser.versionNumber>86));
-                Common.Analytics.trackEvent('Print');
+                var printCallback = function() {
+                    api.asc_Print(new Asc.asc_CDownloadOptions(null, $.browser.chrome || $.browser.safari || $.browser.opera || $.browser.mozilla && $.browser.versionNumber>86));
+                    Common.Analytics.trackEvent('Print');
+                };
+
+                var submitPassword = function(val) {
+                    if(api.asc_CheckPrintPassword(val)) {
+                        printCallback();
+                    } else {
+                        common.controller.modals.createDlgPrintPassword(submitPassword, true);
+                    }
+                };
+
+                if(api && api.asc_CheckPrintPassword && api.asc_CheckPrintPassword() === false) {
+                    common.controller.modals.createDlgPrintPassword(submitPassword);
+                } else {
+                    printCallback();
+                }
             });
 
         DE.ApplicationView.tools.get('#idt-close')
